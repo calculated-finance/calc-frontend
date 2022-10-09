8b7a682cbe0d75fb9c6746a901e5aa0df07f32d1
dougcalc <doug@calculated.fi>
2022-10-09 11:17:26 +0800

    fix type error

--- src/hooks/handleContractQueryError.ts
+++ src/hooks/handleContractQueryError.ts
@@ -1,13 +1,3 @@
-import { appToast } from 'src/pages/_app.page';
-
 export default function handleContractQueryError(error: Error) {
   console.error(error);
-  appToast({
-    status: 'error',
-    title: 'There was an error',
-    position: 'top-right',
-    description: error.message,
-    duration: 9000,
-    isClosable: true,
-  });
 }
