4867c0b0e0b76c35eda5fdd9f847dbed5b2755b2
dougcalc <doug@calculated.fi>
2022-10-10 16:35:50 +0800

    deploy to production

---
+++ infrastructure/production/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "tf-state-ap-southeast-2-503097572706"
+    key            = "terraform/infrastructure/calc-frontend"
+    region         = "ap-southeast-2"
+    dynamodb_table = "terraform-state-locks"
+  }
+}
