4ee13ab52923170c16dddfe228ad1f673ddd9896
dougcalc <doug@calculated.fi>
2022-10-21 08:58:58 +0800

    ignore cloudfront for now

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -20,126 +20,126 @@ resource "aws_s3_bucket_acl" "website_bucket_acl" {
 }
 
 
-resource "aws_cloudfront_distribution" "s3_distribution" {
-  origin {
-    domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
-    origin_id   = "${var.environment}${var.bucket_domain_name}"
+# resource "aws_cloudfront_distribution" "s3_distribution" {
+#   origin {
+#     domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
+#     origin_id   = "${var.environment}${var.bucket_domain_name}"
 
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 80
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = ["TLSv1.2"]
-    }
-  }
-
-
-
-  enabled             = true
-  is_ipv6_enabled     = true
-  comment             = "Some comment"
-  default_root_object = "index.html"
-
-  # logging_config {
-  #   include_cookies = false
-  #   bucket          = "mylogs.s3.amazonaws.com"
-  #   prefix          = "myprefix"
-  # }
-
-  # aliases = ["mysite.example.com", "yoursite.example.com"]
-
-  default_cache_behavior {
-    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "${var.environment}${var.bucket_domain_name}"
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    viewer_protocol_policy = "allow-all"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
-
-  # # Cache behavior with precedence 0
-  # ordered_cache_behavior {
-  #   path_pattern     = "/content/immutable/*"
-  #   allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-  #   cached_methods   = ["GET", "HEAD", "OPTIONS"]
-  #   target_origin_id = local.s3_origin_id
-
-  #   forwarded_values {
-  #     query_string = false
-  #     headers      = ["Origin"]
-
-  #     cookies {
-  #       forward = "none"
-  #     }
-  #   }
-
-  #   min_ttl                = 0
-  #   default_ttl            = 86400
-  #   max_ttl                = 31536000
-  #   compress               = true
-  #   viewer_protocol_policy = "redirect-to-https"
-  # }
-
-  # # Cache behavior with precedence 1
-  # ordered_cache_behavior {
-  #   path_pattern     = "/content/*"
-  #   allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-  #   cached_methods   = ["GET", "HEAD"]
-  #   target_origin_id = local.s3_origin_id
-
-  #   forwarded_values {
-  #     query_string = false
-
-  #     cookies {
-  #       forward = "none"
-  #     }
-  #   }
-
-  #   min_ttl                = 0
-  #   default_ttl            = 3600
-  #   max_ttl                = 86400
-  #   compress               = true
-  #   viewer_protocol_policy = "redirect-to-https"
-  # }
-
-  price_class = "PriceClass_All"
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
-  # tags = {
-  #   Environment = "production"
-  # }
+#     # custom_origin_config {
+#     #   http_port              = 80
+#     #   https_port             = 80
+#     #   origin_protocol_policy = "http-only"
+#     #   origin_ssl_protocols   = ["TLSv1.2"]
+#     # }
+#   }
 
 
-  # custom_error_response {
-  #   error_caching_min_ttl = 300
-  #   error_code            = 403
-  #   response_code         = 200
-  #   response_page_path    = "/index.html"
-  # }
 
-  # custom_error_response {
-  #   error_caching_min_ttl = 300
-  #   error_code            = 404
-  #   response_code         = 200
-  #   response_page_path    = "/index.html"
-  # }
+#   enabled             = true
+#   is_ipv6_enabled     = true
+#   comment             = "Some comment"
+#   default_root_object = "index.html"
 
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
-}
+#   # logging_config {
+#   #   include_cookies = false
+#   #   bucket          = "mylogs.s3.amazonaws.com"
+#   #   prefix          = "myprefix"
+#   # }
+
+#   # aliases = ["mysite.example.com", "yoursite.example.com"]
+
+#   default_cache_behavior {
+#     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+#     cached_methods   = ["GET", "HEAD"]
+#     target_origin_id = "${var.environment}${var.bucket_domain_name}"
+
+#     forwarded_values {
+#       query_string = false
+
+#       cookies {
+#         forward = "none"
+#       }
+#     }
+
+#     viewer_protocol_policy = "allow-all"
+#     min_ttl                = 0
+#     default_ttl            = 3600
+#     max_ttl                = 86400
+#   }
+
+#   # # Cache behavior with precedence 0
+#   # ordered_cache_behavior {
+#   #   path_pattern     = "/content/immutable/*"
+#   #   allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+#   #   cached_methods   = ["GET", "HEAD", "OPTIONS"]
+#   #   target_origin_id = local.s3_origin_id
+
+#   #   forwarded_values {
+#   #     query_string = false
+#   #     headers      = ["Origin"]
+
+#   #     cookies {
+#   #       forward = "none"
+#   #     }
+#   #   }
+
+#   #   min_ttl                = 0
+#   #   default_ttl            = 86400
+#   #   max_ttl                = 31536000
+#   #   compress               = true
+#   #   viewer_protocol_policy = "redirect-to-https"
+#   # }
+
+#   # # Cache behavior with precedence 1
+#   # ordered_cache_behavior {
+#   #   path_pattern     = "/content/*"
+#   #   allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+#   #   cached_methods   = ["GET", "HEAD"]
+#   #   target_origin_id = local.s3_origin_id
+
+#   #   forwarded_values {
+#   #     query_string = false
+
+#   #     cookies {
+#   #       forward = "none"
+#   #     }
+#   #   }
+
+#   #   min_ttl                = 0
+#   #   default_ttl            = 3600
+#   #   max_ttl                = 86400
+#   #   compress               = true
+#   #   viewer_protocol_policy = "redirect-to-https"
+#   # }
+
+#   price_class = "PriceClass_All"
+
+#   restrictions {
+#     geo_restriction {
+#       restriction_type = "none"
+#     }
+#   }
+
+#   # tags = {
+#   #   Environment = "production"
+#   # }
+
+
+#   # custom_error_response {
+#   #   error_caching_min_ttl = 300
+#   #   error_code            = 403
+#   #   response_code         = 200
+#   #   response_page_path    = "/index.html"
+#   # }
+
+#   # custom_error_response {
+#   #   error_caching_min_ttl = 300
+#   #   error_code            = 404
+#   #   response_code         = 200
+#   #   response_page_path    = "/index.html"
+#   # }
+
+#   viewer_certificate {
+#     cloudfront_default_certificate = true
+#   }
+# }
