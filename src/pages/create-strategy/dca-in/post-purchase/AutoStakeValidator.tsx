d6b31ba0135862f61818c6d74490c75d83011650
aidan <aidan@calculated.fi>
2022-10-30 15:38:07 +1100

    filter jailed validators

--- src/pages/create-strategy/dca-in/post-purchase/AutoStakeValidator.tsx
+++ src/pages/create-strategy/dca-in/post-purchase/AutoStakeValidator.tsx
@@ -60,13 +60,15 @@ export default function AutoStakeValidator() {
     },
   );
 
-  const validatorOptions = data?.validators?.map(
+  const validatorOptions = data?.validators?.filter((v: any) => v.jailed == false).map(
     (validator: any) =>
       ({
         value: validator.operator_address,
         label: validator.description.moniker,
+        jailed: validator.jailed
       } || []),
   );
+  console.log(validatorOptions)
   return (
     <FormControl
       isInvalid={Boolean(meta.touched && meta.error)}
