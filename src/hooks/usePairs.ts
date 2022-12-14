303c97c1fe9ac7694b1a5adaaf59703d13316145
dougcalc <112316488+dougcalc@users.noreply.github.com>
2022-12-14 14:01:59 +1100

    disable usk/weth (#125)
    * disable usk
    
    * list denoms alphabetically

--- src/hooks/usePairs.ts
+++ src/hooks/usePairs.ts
@@ -1,3 +1,4 @@
+import getDenomInfo from '@utils/getDenomInfo';
 import { SUPPORTED_DENOMS } from '@utils/SUPPORTED_DENOMS';
 import { useWallet } from '@wizard-ui/react';
 import { CONTRACT_ADDRESS } from 'src/constants';
@@ -6,43 +7,46 @@ import { Denom } from '../models/Denom';
 import { Pair } from '../models/Pair';
 import useQueryWithNotification from './useQueryWithNotification';
 
+const hiddenPairs = ['kujira17w9r23r8v8r7z5lphwj99296fhlye9ej5nq3hlqw554u63m88avspdl9tc'];
+
 function isSupportedDenom(denom: Denom) {
   return SUPPORTED_DENOMS.includes(denom);
 }
 
-// function orderAlphabetically(denoms: Denom[]) {
-//   return denoms.sort((a, b) => {
-//     const { name: nameA } = getDenomInfo(a);
-//     return a.localeCompare(b);
-//   });
-// }
+function orderAlphabetically(denoms: Denom[]) {
+  return denoms.sort((a, b) => {
+    const { name: nameA } = getDenomInfo(a);
+    const { name: nameB } = getDenomInfo(b);
+    return nameA.localeCompare(nameB);
+  });
+}
 
 export function uniqueQuoteDenoms(pairs: Pair[] | undefined) {
-  return Array.from(new Set(pairs?.map((pair) => pair.quote_denom))).filter((denom) =>
+  return orderAlphabetically(Array.from(new Set(pairs?.map((pair) => pair.quote_denom))).filter((denom) =>
     SUPPORTED_DENOMS.includes(denom),
-  );
+  ));
 }
 
 export function uniqueBaseDenoms(pairs: Pair[] | undefined) {
-  return Array.from(new Set(pairs?.map((pair) => pair.base_denom))).filter(isSupportedDenom);
+  return orderAlphabetically(Array.from(new Set(pairs?.map((pair) => pair.base_denom))).filter(isSupportedDenom));
 }
 
 export function uniqueBaseDenomsFromQuoteDenom(initialDenom: Denom, pairs: Pair[] | undefined) {
-  return Array.from(
+  return orderAlphabetically(Array.from(
     new Set(pairs?.filter((pair) => pair.quote_denom === initialDenom).map((pair) => pair.base_denom)),
-  ).filter(isSupportedDenom);
+  ).filter(isSupportedDenom));
 }
 
 export function uniqueQuoteDenomsFromBaseDenom(resultingDenom: Denom, pairs: Pair[] | undefined) {
-  return Array.from(
+  return orderAlphabetically(Array.from(
     new Set(pairs?.filter((pair) => pair.base_denom === resultingDenom).map((pair) => pair.quote_denom)),
-  ).filter(isSupportedDenom);
+  ).filter(isSupportedDenom));
 }
 
 export default function usePairs() {
   const { client } = useWallet();
 
-  return useQueryWithNotification<PairsResponse>(
+  const queryResult = useQueryWithNotification<PairsResponse>(
     ['pairs', client],
     async () => {
       const result = await client!.queryContractSmart(CONTRACT_ADDRESS, {
@@ -54,4 +58,10 @@ export default function usePairs() {
       enabled: !!client,
     },
   );
+  return {
+    ...queryResult,
+    data: {
+      pairs: queryResult.data?.pairs.filter((pair) => !hiddenPairs.includes(pair.address)),
+    }
+  }
 }
