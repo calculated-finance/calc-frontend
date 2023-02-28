645f9ced9a2e20b619201a38dc2aa804b9190fa8
dougcalc <doug@calculated.fi>
2023-02-28 15:09:45 +0800

    refactor agreement form

---
+++ src/components/Summary/SummaryAgreementForm.tsx
@@ -0,0 +1,61 @@
+import { Button, FormControl, FormErrorMessage, useDisclosure, Text } from '@chakra-ui/react';
+import Icon from '@components/Icon';
+import { CheckedIcon } from '@fusion-icons/react/interface';
+import { Form, Formik } from 'formik';
+import Submit from '@components/Submit';
+import { TermsModal } from '@components/TermsModal';
+import { AgreementCheckbox } from '../AgreementCheckbox';
+
+export type AgreementForm = {
+  acceptedAgreement: boolean;
+};
+
+export function SummaryAgreementForm({
+  isError,
+  error,
+  onSubmit,
+}: {
+  isError: boolean;
+  error: Error | null;
+  onSubmit: () => void;
+}) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const validate = (values: AgreementForm) => {
+    if (!values.acceptedAgreement) {
+      return { acceptedAgreement: 'You must accept the terms and conditions before continuing.' };
+    }
+
+    return {};
+  };
+
+  return (
+    <Formik initialValues={{ acceptedAgreement: false }} validate={validate} onSubmit={onSubmit}>
+      <Form>
+        <AgreementCheckbox>
+          <Text textStyle="body-xs">
+            I have read and agree to be bound by the{' '}
+            <Button
+              textDecoration="underline"
+              fontWeight="normal"
+              size="xs"
+              display="inline-flex"
+              colorScheme="blue"
+              variant="unstyled"
+              onClick={onOpen}
+            >
+              CALC Terms & Conditions.
+            </Button>
+          </Text>
+        </AgreementCheckbox>
+        <FormControl isInvalid={isError}>
+          <Submit w="full" type="submit" rightIcon={<Icon as={CheckedIcon} stroke="navy" />}>
+            Confirm
+          </Submit>
+          <FormErrorMessage>Failed to create strategy (Reason: {error?.message})</FormErrorMessage>
+        </FormControl>
+        <TermsModal showCheckbox={false} isOpen={isOpen} onClose={onClose} />
+      </Form>
+    </Formik>
+  );
+}
--- src/pages/create-strategy/dca-in/confirm-purchase/index.page.tsx
+++ src/pages/create-strategy/dca-in/confirm-purchase/index.page.tsx
@@ -1,56 +1,30 @@
-import { Button, Center, FormControl, FormErrorMessage, Stack, useDisclosure, Text } from '@chakra-ui/react';
-import Icon from '@components/Icon';
+import { Divider, Stack } from '@chakra-ui/react';
 import { getFlowLayout } from '@components/Layout';
 import NewStrategyModal, { NewStrategyModalBody, NewStrategyModalHeader } from '@components/NewStrategyModal';
-import { CheckedIcon } from '@fusion-icons/react/interface';
-import { useRouter } from 'next/router';
 import { FormNames, useConfirmForm } from 'src/hooks/useDcaInForm';
 import useCreateVault from '@hooks/useCreateVault';
 import usePageLoad from '@hooks/usePageLoad';
-import { Form, Formik, FormikHelpers } from 'formik';
-import Submit from '@components/Submit';
 import useSteps from '@hooks/useSteps';
 import steps from 'src/formConfig/dcaIn';
-import { TermsModal } from '@components/TermsModal';
 import { TransactionType } from '@components/TransactionType';
-import Summary from './Summary';
-import Fees from '../../../../components/Fees';
-import { AgreementCheckbox } from '../../../../components/AgreementCheckbox';
+import { InvalidData } from '@components/InvalidData';
+import { SummaryAgreementForm } from '@components/Summary/SummaryAgreementForm';
+import DcaDiagram from '@components/DcaDiagram';
+import { SummaryAfterEachSwap } from '@components/Summary/SummaryAfterEachSwap';
+import { SummaryTheSwap } from '@components/Summary/SummaryTheSwap';
+import { SummaryWhileSwapping } from '@components/Summary/SummaryWhileSwapping';
+import { SummaryYourDeposit } from '@components/Summary/SummaryYourDeposit';
 import { getTimeSaved } from '../../../../helpers/getTimeSaved';
+import Fees from '../../../../components/Fees';
 
-function InvalidData() {
-  const router = useRouter();
-  const { actions } = useConfirmForm(FormNames.DcaIn);
-
-  const handleClick = () => {
-    actions.resetAction();
-    router.push('/create-strategy/dca-in/assets');
-  };
-  return (
-    <Center>
-      {/* Better to link to start of specific strategy */}
-      Invalid Data, please&nbsp;
-      <Button onClick={handleClick} variant="link">
-        restart
-      </Button>
-    </Center>
-  );
-}
-
-type AgreementForm = {
-  acceptedAgreement: boolean;
-};
-
-function ConfirmPurchase() {
+function Page() {
   const { state, actions } = useConfirmForm(FormNames.DcaIn);
   const { isPageLoading } = usePageLoad();
-  const { nextStep } = useSteps(steps);
+  const { nextStep, goToStep } = useSteps(steps);
 
-  const { mutate, isError, error } = useCreateVault(FormNames.DcaIn, TransactionType.Buy);
+  const { mutate, isError, error, isLoading } = useCreateVault(FormNames.DcaIn, TransactionType.Buy);
 
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
-  const handleSubmit = (values: AgreementForm, { setSubmitting }: FormikHelpers<AgreementForm>) =>
+  const handleSubmit = () =>
     mutate(undefined, {
       onSuccess: async (strategyId) => {
         await nextStep({
@@ -60,68 +34,41 @@ function ConfirmPurchase() {
         });
         actions.resetAction();
       },
-      onSettled: () => {
-        setSubmitting(false);
-      },
     });
 
-  const validate = (values: AgreementForm) => {
-    if (!values.acceptedAgreement) {
-      return { acceptedAgreement: 'You must accept the terms and conditions before continuing.' };
-    }
-
-    return {};
+  const handleRestart = () => {
+    actions.resetAction();
+    goToStep(0);
   };
 
   return (
-    <>
-      <Formik initialValues={{ acceptedAgreement: false }} validate={validate} onSubmit={handleSubmit}>
-        {({ isSubmitting }) => (
-          <NewStrategyModal>
-            <NewStrategyModalHeader stepsConfig={steps} resetForm={actions.resetAction}>
-              Confirm &amp; Sign
-            </NewStrategyModalHeader>
-            <NewStrategyModalBody stepsConfig={steps} isLoading={isPageLoading} isSigning={isSubmitting}>
-              {state ? (
-                <Form>
-                  <Stack spacing={4}>
-                    <Summary />
-                    <Fees formName={FormNames.DcaIn} />
-                    <AgreementCheckbox>
-                      <Text textStyle="body-xs">
-                        I have read and agree to be bound by the{' '}
-                        <Button
-                          textDecoration="underline"
-                          fontWeight="normal"
-                          size="xs"
-                          display="inline-flex"
-                          colorScheme="blue"
-                          variant="unstyled"
-                          onClick={onOpen}
-                        >
-                          CALC Terms & Conditions.
-                        </Button>
-                      </Text>
-                    </AgreementCheckbox>
-                    <FormControl isInvalid={isError}>
-                      <Submit w="full" type="submit" rightIcon={<Icon as={CheckedIcon} stroke="navy" />}>
-                        Confirm
-                      </Submit>
-                      <FormErrorMessage>Failed to create strategy (Reason: {error?.message})</FormErrorMessage>
-                    </FormControl>
-                  </Stack>
-                </Form>
-              ) : (
-                <InvalidData />
-              )}
-            </NewStrategyModalBody>
-          </NewStrategyModal>
+    <NewStrategyModal>
+      <NewStrategyModalHeader stepsConfig={steps} resetForm={actions.resetAction}>
+        Confirm &amp; Sign
+      </NewStrategyModalHeader>
+      <NewStrategyModalBody stepsConfig={steps} isLoading={isPageLoading} isSigning={isLoading}>
+        {state ? (
+          <Stack spacing={4}>
+            <DcaDiagram
+              initialDenom={state.initialDenom}
+              resultingDenom={state.resultingDenom}
+              initialDeposit={state.initialDeposit}
+            />
+            <Divider />
+            <SummaryYourDeposit state={state} />
+            <SummaryTheSwap state={state} />
+            <SummaryWhileSwapping state={state} />
+            <SummaryAfterEachSwap state={state} />
+            <Fees formName={FormNames.DcaIn} />
+            <SummaryAgreementForm isError={isError} error={error} onSubmit={handleSubmit} />
+          </Stack>
+        ) : (
+          <InvalidData onRestart={handleRestart} />
         )}
-      </Formik>
-      <TermsModal showCheckbox={false} isOpen={isOpen} onClose={onClose} />
-    </>
+      </NewStrategyModalBody>
+    </NewStrategyModal>
   );
 }
-ConfirmPurchase.getLayout = getFlowLayout;
+Page.getLayout = getFlowLayout;
 
-export default ConfirmPurchase;
+export default Page;
