a4d9e53c42451d11c3fdd85cc51669846fa3d718
Nathaniel Giabardo <nathaniel@Nathaniels-MBP.gateway>
2023-03-03 16:41:40 +1100

    added install links for wallet

--- src/components/WalletListItem.tsx
+++ src/components/WalletListItem.tsx
@@ -1,18 +1,22 @@
 import { WalletReadyState } from '@wizard-ui/core';
 import type { Wallet } from '@wizard-ui/react';
 
-import { Center, Spacer, Image } from '@chakra-ui/react';
+import { Center, Spacer, Image, Box, Button } from '@chakra-ui/react';
 
 export interface WalletListItemProps {
   handleClick: () => void;
   wallet: Wallet;
+  walletInstallLink: string;
 }
 
-export function WalletListItem({ handleClick, wallet }: WalletListItemProps) {
+export function WalletListItem({ handleClick, wallet, walletInstallLink }: WalletListItemProps) {
+  const isInstalled = wallet.readyState === WalletReadyState.Installed;
+
   return (
     <Center
+      as={isInstalled ? undefined : 'a'}
+      onClick={isInstalled ? handleClick : undefined}
       w="full"
-      onClick={handleClick}
       bg="deepHorizon"
       p={2}
       px={3}
@@ -21,11 +25,14 @@ export function WalletListItem({ handleClick, wallet }: WalletListItemProps) {
       borderWidth={1}
       _hover={{ bg: 'abyss.200' }}
       cursor="pointer"
+      href={walletInstallLink}
+      target={isInstalled ? undefined : '_blank'}
+      rel={isInstalled ? undefined : 'noopener noreferrer'}
     >
       <Image w={4} mr={2} src={wallet.adapter.icon} alt={`${wallet.adapter.name} icon`} />
       <div>{wallet.adapter.name}</div>
       <Spacer />
-      {wallet.readyState === WalletReadyState.Installed && <p>Installed</p>}
+      <p>{isInstalled ? 'Installed' : 'Install'}</p>
     </Center>
   );
 }
--- src/components/WalletModal.tsx
+++ src/components/WalletModal.tsx
@@ -18,6 +18,7 @@ import {
   Stack,
   Text,
   useDisclosure,
+  ModalFooter,
 } from '@chakra-ui/react';
 import { useWallet, Wallet } from '@wizard-ui/react';
 import { FiChevronUp, FiChevronDown } from 'react-icons/fi';
@@ -31,25 +32,6 @@ function WalletModal() {
 
   const { isOpen, onToggle } = useDisclosure();
 
-  const [installedWallets] = useMemo(() => {
-    const installed: Wallet[] = [];
-    const notDetected: Wallet[] = [];
-    const loadable: Wallet[] = [];
-
-    // eslint-disable-next-line no-restricted-syntax
-    for (const wallet of wallets) {
-      if (wallet.readyState === WalletReadyState.NotDetected) {
-        notDetected.push(wallet);
-      } else if (wallet.readyState === WalletReadyState.Loadable) {
-        loadable.push(wallet);
-      } else if (wallet.readyState === WalletReadyState.Installed) {
-        installed.push(wallet);
-      }
-    }
-
-    return [installed, [...loadable, ...notDetected]];
-  }, [wallets]);
-
   const handleClose = useCallback(() => {
     setVisible(false);
   }, [setVisible]);
@@ -61,6 +43,10 @@ function WalletModal() {
     },
     [select, handleClose],
   );
+
+  // const isMobile = useCallback(() => {
+  //   /Mobi/.test(navigator.userAgent);
+  // }, []);
 
   return (
     <Modal isOpen={visible || connecting} onClose={handleClose} size="sm">
@@ -80,21 +66,17 @@ function WalletModal() {
             <ModalHeader textAlign="center">Connect wallet</ModalHeader>
             <ModalBody>
               <Stack spacing={6}>
-                {installedWallets.length ? (
-                  installedWallets.map((wallet) => (
-                    <Box>
-                      <WalletListItem
-                        key={wallet.adapter.name}
-                        handleClick={() => handleWalletClick(wallet.adapter.name)}
-                        wallet={wallet}
-                      />
-                    </Box>
-                  ))
-                ) : (
-                  <Center>
-                    <Text textStyle="body">No wallet extensions found</Text>
-                  </Center>
-                )}
+                {wallets.map((wallet) => (
+                  <Box>
+                    <WalletListItem
+                      key={wallet.adapter.name}
+                      handleClick={() => handleWalletClick(wallet.adapter.name)}
+                      wallet={wallet}
+                      walletInstallLink="https://www.keplr.app/download"
+                    />
+                  </Box>
+                ))}
+
                 <Stack>
                   <Button
                     onClick={onToggle}
