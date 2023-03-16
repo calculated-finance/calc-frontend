19a61c73fad4509fd727c93bf021dcfd67e16d78
Your Name <you@example.com>
2023-03-16 17:29:23 +1100

    fix dca plus performance endpoint

--- src/hooks/useDcaPlusPerformance.ts
+++ src/hooks/useDcaPlusPerformance.ts
@@ -20,9 +20,6 @@ export default function useDcaPlusPerformance(id?: Strategy['id']) {
           vault_id: id,
         },
       } as QueryMsg);
-      if (result.vault.owner !== address) {
-        throw new Error('Strategy not found');
-      }
       return result;
     },
     {
