674ed3a5f71a1d6a50d75222a32b3a8798edc290
Nathaniel Giabardo <nathaniel@192-168-1-106.tpgi.com.au>
2023-03-19 16:49:40 +1100

    confetti Lottie animation and thats-calculated-thinking text

--- src/pages/create-strategy/dca-plus-in-puff-puff/success/index.page.tsx
+++ src/pages/create-strategy/dca-plus-in-puff-puff/success/index.page.tsx
@@ -1,9 +1,17 @@
 import { getFlowLayout } from '@components/Layout';
 import { dcaPlusInSteps } from 'src/formConfig/dcaPlusIn';
 import { SuccessStrategyModal } from '@components/SuccessStrategyModal';
+import React from 'react';
+import { AbsoluteCenter } from '@chakra-ui/react';
+import Lottie from 'lottie-react';
+import Confetti from 'src/animations/confetti.json';
 
 function Success() {
-  return <SuccessStrategyModal stepConfig={dcaPlusInSteps} />;
+  return (
+    <>
+      <SuccessStrategyModal stepConfig={dcaPlusInSteps} />; ;
+    </>
+  );
 }
 Success.getLayout = getFlowLayout;
 
