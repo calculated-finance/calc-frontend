90a8ba4f9d16d25f22d3c32c3adc32839edc3b59
Your Name <you@example.com>
2023-04-24 13:57:26 +1000

    update query

--- src/helpers/strategy/dcaPlus/index.tsx
+++ src/helpers/strategy/dcaPlus/index.tsx
@@ -21,12 +21,21 @@ import { Chains, useChainStore } from '@hooks/useChain';
 
 function getDcaPlusConfig(strategy: Strategy | StrategyOsmosis) {
   const { chain } = useChainStore.getState();
-  const dca_plus_config =
-    chain === Chains.Kujira
-      ? (strategy as Strategy).dca_plus_config
-      : (strategy as StrategyOsmosis).swap_adjustment_strategy?.dca_plus;
+  if (chain === Chains.Osmosis) {
+    const osmosisStrategy = strategy as StrategyOsmosis;
+    return {
+      total_deposit: osmosisStrategy.deposited_amount,
+      standard_dca_received_amount:
+        osmosisStrategy.performance_assessment_strategy?.compare_to_standard_dca.received_amount,
+      standard_dca_swapped_amount:
+        osmosisStrategy.performance_assessment_strategy?.compare_to_standard_dca.swapped_amount,
+      escrow_level: osmosisStrategy.escrow_level,
+      escrowed_balance: osmosisStrategy.escrowed_amount,
+      model_id: osmosisStrategy.swap_adjustment_strategy?.dca_plus.model_id,
+    };
+  }
 
-  return dca_plus_config;
+  return (strategy as Strategy).dca_plus_config;
 }
 
 export function getStandardDcaTotalReceived(strategy: Strategy) {
