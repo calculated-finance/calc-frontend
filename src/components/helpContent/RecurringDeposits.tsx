72d091022dc1f9089d423d0a326a3acce901b0a3
Your Name <you@example.com>
2023-05-04 09:25:01 +1000

    copy chhanges

--- src/components/helpContent/RecurringDeposits.tsx
+++ src/components/helpContent/RecurringDeposits.tsx
@@ -16,7 +16,7 @@ export function RecurringDeposits() {
       </Text>
 
       <Text>
-        This is estimated to be Q1 2023 but may happen sooner. Please follow{' '}
+        This is estimated to be Q4 2023 but may happen sooner. Please follow{' '}
         <Link isExternal href="https://twitter.com/CALC_Finance">
           @CALC_finance
         </Link>{' '}
