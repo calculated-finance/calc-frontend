1f571d6857fe02dd2b0abf257f162247498dd88c
Your Name <you@example.com>
2023-05-07 10:31:51 +1000

    update sentry

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -6,25 +6,9 @@ import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
   dsn: 'https://aafc272787584d989a7a22a79348a4c2@o4505139619364864.ingest.sentry.io/4505139621462016',
+  integrations: [new Sentry.BrowserTracing()],
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-
-  replaysOnErrorSampleRate: 1.0,
-
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
-
-  // You can remove this option if you're not planning to use the Sentry Session Replay feature:
-  integrations: [
-    new Sentry.Replay({
-      // Additional Replay configuration goes in here, for example:
-      maskAllText: true,
-      blockAllMedia: true,
-    }),
-  ],
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
 });
