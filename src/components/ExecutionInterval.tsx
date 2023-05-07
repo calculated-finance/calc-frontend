d9b4079d9480f4e265544660eaaf981933a290a3
Your Name <you@example.com>
2023-05-07 12:06:23 +1000

    wip

--- src/components/ExecutionInterval.tsx
+++ src/components/ExecutionInterval.tsx
@@ -1,11 +1,26 @@
-import { FormControl, FormLabel, useRadioGroup } from '@chakra-ui/react';
+import {
+  FormControl,
+  FormLabel,
+  Text,
+  SimpleGrid,
+  Spacer,
+  useRadioGroup,
+  Flex,
+  Stack,
+  HStack,
+  FormErrorMessage,
+} from '@chakra-ui/react';
 import { useField } from 'formik';
-import { featureFlags } from 'src/constants';
 import { executionIntervalData } from '@helpers/executionIntervalData';
+import { Chains, useChain } from '@hooks/useChain';
+import { FiCalendar, FiClock } from 'react-icons/fi';
+import { featureFlags } from 'src/constants';
 import Radio from './Radio';
 import RadioCard from './RadioCard';
+import NumberInput from './NumberInput';
+import Select from './Select';
 
-export default function ExecutionInterval() {
+function ExecutionIntervalLegacy() {
   const [field, , helpers] = useField({ name: 'executionInterval' });
 
   const { getRootProps, getRadioProps } = useRadioGroup({
@@ -31,4 +46,66 @@ export default function ExecutionInterval() {
       </Radio>
     </FormControl>
   );
+}
+function TimePeriodOption({ label, icon }: { label: string; icon: any }) {
+  return (
+    <HStack flexGrow={1}>
+      {icon}
+      <Text>{label}</Text>
+    </HStack>
+  );
+}
+const timePeriodOptions = [
+  { value: 'minute', label: <TimePeriodOption label="Minute(s)" icon={<FiClock />} /> },
+  { value: 'hour', label: <TimePeriodOption label="Hour(s)" icon={<FiClock />} /> },
+  { value: 'day', label: <TimePeriodOption label="Day(s)" icon={<FiCalendar />} /> },
+  { value: 'week', label: <TimePeriodOption label="Week(s)" icon={<FiCalendar />} /> },
+];
+
+function ExecutionIntervalCustom() {
+  const [{ onChange, ...incrementField }, incrementMeta, incrementHelpers] = useField({
+    name: 'executionIntervalIncrement',
+  });
+  const [periodField, periodMeta, periodHelpers] = useField({ name: 'executionIntervalPeriod' });
+
+  return (
+    <FormControl>
+      <FormLabel>CALC will swap for you, every:</FormLabel>
+      <Stack>
+        <Flex>
+          <Text textStyle="body-xs">Increment</Text>
+          <Spacer />
+          <Text textStyle="body-xs">Time period</Text>
+        </Flex>
+        <SimpleGrid columns={2} spacing={2}>
+          <FormControl isInvalid={incrementMeta.touched && !!incrementMeta.error}>
+            <NumberInput
+              onChange={incrementHelpers.setValue}
+              placeholder="Enter increment"
+              {...incrementField}
+              decimalScale={0}
+            />
+            <FormErrorMessage>{incrementMeta.touched && incrementMeta.error}</FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={periodMeta.touched && !!periodMeta.error}>
+            <Select
+              value={periodField.value}
+              options={timePeriodOptions}
+              placeholder="Select period"
+              onChange={periodHelpers.setValue}
+            />
+            <FormErrorMessage>{periodMeta.touched && periodMeta.error}</FormErrorMessage>
+          </FormControl>
+        </SimpleGrid>
+      </Stack>
+    </FormControl>
+  );
+}
+
+export default function ExecutionInterval() {
+  const { chain } = useChain();
+  if (chain === Chains.Osmosis) {
+    return <ExecutionIntervalCustom />;
+  }
+  return <ExecutionIntervalLegacy />;
 }
