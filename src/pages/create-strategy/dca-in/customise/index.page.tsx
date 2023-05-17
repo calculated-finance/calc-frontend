308d2c57276bfdc92d76f83ea041332e8d55cde6
Nathaniel Giabardo <nathaniel@Nathaniels-MBP.home>
2023-05-17 12:15:54 +0100

    removed unnecessary code

--- src/pages/create-strategy/dca-in/customise/index.page.tsx
+++ src/pages/create-strategy/dca-in/customise/index.page.tsx
@@ -76,7 +76,7 @@ function DcaInStep2() {
                 />
                 <AdvancedSettingsSwitch />
                 <TriggerForm transactionType={TransactionType.Buy} formName={FormNames.DcaIn} />
-                <ExecutionInterval transactionType={TransactionType.Buy} />
+                <ExecutionInterval />
                 <SwapAmount step1State={state.step1} />
                 <Collapse in={values.advancedSettings}>
                   <Box m="px">
