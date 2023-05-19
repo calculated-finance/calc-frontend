63370d71e94a7c7bc73a6fa011a70fc92770ac0c
fluffydonkey <111311384+fluffydonkey@users.noreply.github.com>
2023-05-19 17:23:11 +0100

    add logic to get correct asset list (#311)

--- src/hooks/useAssetList.tsx
+++ src/hooks/useAssetList.tsx
@@ -1,14 +1,20 @@
 import 'isomorphic-fetch';
 import useQueryWithNotification from '@hooks/useQueryWithNotification';
 import { Asset, AssetList } from '@chain-registry/types';
+import { isMainnet } from '@utils/isMainnet';
 
 export function findAsset(assets: Asset[], denom: string | undefined) {
   return assets.find((asset) => asset.base === denom);
 }
 
 export function useAssetList(): { isLoading: any; error: any; data: any } {
+
+  const baseUrl = 'https://raw.githubusercontent.com/osmosis-labs/assetlists/main'
+
+  const chainIdentifier = isMainnet() ? 'osmosis-1/osmosis-1' : 'osmo-test-4/osmo-test-4'
+
   return useQueryWithNotification<AssetList>(['assetList'], () =>
-    fetch('https://raw.githubusercontent.com/osmosis-labs/assetlists/main/osmo-test-4/osmo-test-4.assetlist.json').then(
+    fetch(`${baseUrl}/${chainIdentifier}.assetlist.json`).then(
       (res) => res.json(),
     ),
   );
