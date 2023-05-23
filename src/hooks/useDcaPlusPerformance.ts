b050c697d1856cd79b07bb4d1b0a64d66620cc37
Your Name <you@example.com>
2023-05-23 16:10:07 +0100

    fix query

--- src/hooks/useDcaPlusPerformance.ts
+++ src/hooks/useDcaPlusPerformance.ts
@@ -3,42 +3,42 @@ import { QueryMsg } from 'src/interfaces/v1/generated/query';
 import { QueryMsg as QueryMsgOsmosis } from 'src/interfaces/generated-osmosis/query';
 import { DcaPlusPerformanceResponse } from 'src/interfaces/v1/generated/response/get_dca_plus_performance';
 import { getChainContractAddress } from '@helpers/chains';
-import { isStrategyV2 } from '@helpers/strategy/isStrategyV2';
 import useQueryWithNotification from './useQueryWithNotification';
-import { Strategy, StrategyOsmosis } from './useStrategies';
-import { Chains, useChain } from './useChain';
+import { Strategy } from './useStrategies';
+import { useChain } from './useChain';
 import { useCosmWasmClient } from './useCosmWasmClient';
+import { useVersion } from './useVersion';
 
-export default function useDcaPlusPerformance(strategy: Strategy | StrategyOsmosis, enabled: boolean) {
+export default function useDcaPlusPerformance(id: Strategy['id'], enabled: boolean) {
   const { address } = useWallet();
   const { chain } = useChain();
   const client = useCosmWasmClient((state) => state.client);
-
-  const { id } = strategy;
+  const version = useVersion();
 
   return useQueryWithNotification<DcaPlusPerformanceResponse>(
-    ['strategy-dca-plus-performance', id, client, address],
+    ['strategy-dca-plus-performance', id, client, address, version],
     async () => {
       if (!client) {
         throw new Error('No client');
       }
-      const msg = isStrategyV2(strategy)
-        ? ({
-            get_vault_performance: {
-              vault_id: id,
-            },
-          } as QueryMsgOsmosis)
-        : ({
-            get_dca_plus_performance: {
-              vault_id: id,
-            },
-          } as QueryMsg);
+      const msg =
+        version === 'v2'
+          ? ({
+              get_vault_performance: {
+                vault_id: id,
+              },
+            } as QueryMsgOsmosis)
+          : ({
+              get_dca_plus_performance: {
+                vault_id: id,
+              },
+            } as QueryMsg);
 
       const result = await client.queryContractSmart(getChainContractAddress(chain!), msg);
       return result;
     },
     {
-      enabled: !!client && !!id && !!address && !!enabled && !!chain,
+      enabled: !!client && !!id && !!address && !!enabled && !!chain && !!version,
     },
   );
 }
