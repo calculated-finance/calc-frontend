d90e9345e4eb6f6a16cdeeea19776ade21fa3052
Your Name <you@example.com>
2023-05-26 12:06:58 +0100

    fix typo

--- src/hooks/useAssetList.tsx
+++ src/hooks/useAssetList.tsx
@@ -3,7 +3,7 @@ import useQueryWithNotification from '@hooks/useQueryWithNotification';
 import { Asset, AssetList } from '@chain-registry/types';
 import * as Sentry from '@sentry/react';
 import { isMainnet } from '@utils/isMainnet';
-import tesnetAssetList from 'src/assetLists/osmo-test-5.assetlist.json';
+import testnetAssetList from 'src/assetLists/osmo-test-5.assetlist.json';
 import mainnetAssetList from 'src/assetLists/osmosis-1.assetlist.json';
 import { Chains, useChain } from './useChain';
 
@@ -17,7 +17,7 @@ export function useAssetList(): { isLoading: any; error: any; data: any } {
 
   const chainIdentifier = isMainnet() ? 'osmosis-1/osmosis-1' : 'osmo-test-5/osmo-test-5';
 
-  const backup = isMainnet() ? mainnetAssetList : tesnetAssetList;
+  const backup = isMainnet() ? mainnetAssetList : testnetAssetList;
 
   return useQueryWithNotification<AssetList>(
     ['assetList'],
