6a840016e150db7b310f77224746a64fe16e1d55
Your Name <you@example.com>
2023-05-29 13:40:47 +0100

    fix dca plus assets in osmosis

--- src/utils/SUPPORTED_DENOMS.ts
+++ src/utils/SUPPORTED_DENOMS.ts
@@ -6,10 +6,3 @@ export const SUPPORTED_DENOMS = Object.keys(isMainnet() ? mainnetDenoms : testne
   const { enabled } = getDenomInfo(denom);
   return enabled;
 });
-
-export const SUPPORTED_DENOMS_FOR_DCA_PLUS = Object.keys(isMainnet() ? mainnetDenoms : testnetDenoms).filter(
-  (denom) => {
-    const { enabledInDcaPlus } = getDenomInfo(denom);
-    return enabledInDcaPlus;
-  },
-);
