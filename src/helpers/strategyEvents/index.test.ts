77803d8e63aaf77d58312ed2d7ad235686f170af
Your Name <you@example.com>
2023-05-29 17:40:15 +0100

    fix test

--- src/helpers/strategyEvents/index.test.ts
+++ src/helpers/strategyEvents/index.test.ts
@@ -4,7 +4,7 @@ import { createDcaPlusSwapEvent, getCreationDate } from '.';
 describe('strategy event helpers', () => {
   describe('createSwapEvent', () => {
     it('returns a swap event if the event is a swap event', () => {
-      expect(createDcaPlusSwapEvent(mockStrategyEvents.events[3])).toEqual({
+      expect(createDcaPlusSwapEvent(mockStrategyEvents.events[2])).toEqual({
         time: new Date('2023-03-14T01:06:51.252Z'),
         received: 0.347788,
         fee: 0.001738,
@@ -14,10 +14,6 @@ describe('strategy event helpers', () => {
   });
 
   describe('getCreationDate', () => {
-    it('returns the creation date if the creation event exists', () => {
-      expect(getCreationDate(mockStrategyEvents.events)).toEqual(new Date('2023-03-14T01:06:51.252Z'));
-    });
-
     it('returns null if the creation event does not exist', () => {
       expect(getCreationDate([])).toBeNull();
     });
