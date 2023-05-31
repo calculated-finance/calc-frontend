87300813583c9cf72bfd098d12b070ec7e14fadc
natecalc <125798762+natecalc@users.noreply.github.com>
2023-05-31 19:00:06 +0100

    updated numbers less than 0.1 to 4 decimals (#353)
    Co-authored-by: Nathaniel Giabardo <nathaniel@Nathaniels-MBP.home>

--- src/helpers/format/formatFiat.tsx
+++ src/helpers/format/formatFiat.tsx
@@ -1,7 +1,11 @@
 import { isNaN } from 'lodash';
 
 export function formatFiat(value: number) {
-  return `${new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(
-    !isNaN(value) ? value : 0,
-  )} USD`;
+  const roundedValue = Math.round(value * 100) / 100;
+  return `${new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: roundedValue < 0.1 ? 4 : 2,
+    currency: 'USD',
+  }).format(!isNaN(value) ? value : 0)} USD`;
 }
