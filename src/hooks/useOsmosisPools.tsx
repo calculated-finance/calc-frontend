afb502af0c605726070431b0786c7fe51e4ccc9e
Your Name <you@example.com>
2023-06-02 14:08:13 +0100

    fix types

--- src/hooks/useOsmosisPools.tsx
+++ src/hooks/useOsmosisPools.tsx
@@ -1,27 +1,33 @@
 import Long from 'long';
 import { osmosis } from 'osmojs';
 import useQueryWithNotification from '@hooks/useQueryWithNotification';
+import { Pool } from 'osmojs/types/codegen/osmosis/gamm/pool-models/balancer/balancerPool';
+import { QueryPoolsResponse } from 'osmojs/types/codegen/osmosis/concentrated-liquidity/pool-model/query';
 import { useOsmosis } from './useOsmosis';
 
 const POOLS_QUERY_LIMIT = 800;
 
-async function fetchPoolsRecursively(queryClient: any, allPools = [] as any[], injectedKey = []): Promise<any[]> {
-  const key = injectedKey.length ? injectedKey : new Uint8Array([]);
-  const poolsResponse = await queryClient?.osmosis.gamm.v1beta1.pools({
+async function fetchPoolsRecursively(
+  queryClient: any,
+  allPools: Pool[],
+  injectedKey: Uint8Array | undefined,
+): Promise<Pool[]> {
+  const key = injectedKey?.length ? injectedKey : new Uint8Array([]);
+  const poolsResponse = (await queryClient?.osmosis.gamm.v1beta1.pools({
     pagination: {
       limit: Long.fromNumber(POOLS_QUERY_LIMIT, true),
       key,
       offset: Long.fromNumber(0, true),
     },
-  });
+  })) as QueryPoolsResponse;
   const { nextKey } = poolsResponse?.pagination || {};
 
-  const decodedPools = await poolsResponse?.pools.map((pool: any) => {
+  const decodedPools = await poolsResponse?.pools.map((pool) => {
     try {
       const decodedPool = osmosis.gamm.v1beta1.Pool.decode(pool.value);
       return decodedPool;
     } catch (e) {
-      return null;
+      throw new Error(`Failed to decode pool: ${e}`);
     }
   });
   allPools.push(...decodedPools);
@@ -38,7 +44,7 @@ export function useOsmosisPools(enabled = true) {
 
   return useQueryWithNotification(
     ['pools-for-denom'],
-    () => fetchPoolsRecursively(query),
+    () => fetchPoolsRecursively(query, [], undefined),
 
     { enabled: !!query && enabled },
   );
