80a6d73ace1a744b92ac03b33af81a823188cd8b
Your Name <you@example.com>
2023-06-21 15:25:28 +0100

    fix chain state

--- src/hooks/useChain/index.ts
+++ src/hooks/useChain/index.ts
@@ -1,5 +1,4 @@
 import { useCosmWasmClient } from '@hooks/useCosmWasmClient';
-import * as Sentry from '@sentry/react';
 import { useFormStore } from '@hooks/useFormStore';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useRouter } from 'next/router';
@@ -30,42 +29,40 @@ export const useChain = () => {
   const storedChain = useChainStore((state) => state.chain);
   const setStoredChain = useChainStore((state) => state.setChain);
 
-
-
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState<ChainState>({} as ChainState);
 
-  const updateQueryParam = useCallback((newChain: Chains) => {
-    router.replace({
-      pathname: router.pathname,
-      query: { ...router.query, chain: newChain },
-    });
-  }, [router]);
+  const updateQueryParam = useCallback(
+    (newChain: Chains) => {
+      router.replace({
+        pathname: router.pathname,
+        query: { ...router.query, chain: newChain },
+      });
+    },
+    [router],
+  );
 
-
-  const setChain = useCallback((newChain: Chains) => {
-    useFormStore.setState({ forms: {} });
-    useCosmWasmClient.setState({ client: null });
-    setStoredChain(newChain);
-    updateQueryParam(newChain)
-  }, [router]);
-
-
+  const setChain = useCallback(
+    (newChain: Chains) => {
+      useFormStore.setState({ forms: {} });
+      useCosmWasmClient.setState({ client: null });
+      setStoredChain(newChain);
+      updateQueryParam(newChain);
+    },
+    [setStoredChain, updateQueryParam],
+  );
 
   useEffect(() => {
     if (router.isReady) {
-      if(chain) {
+      if (chain) {
         setData({ chain: chain as Chains, setChain });
       } else if (storedChain) {
         updateQueryParam(storedChain);
         setData({ chain: storedChain, setChain });
-        Sentry.captureMessage(`No chain set for ${router.pathname}`);
       }
       setIsLoading(false);
     }
-      
-  }, [router.isReady, setChain, chain]);
+  }, [router.isReady, setChain, chain, updateQueryParam, storedChain]);
 
-
-  return {...data as ChainState, isLoading}
+  return { ...(data as ChainState), isLoading };
 };
