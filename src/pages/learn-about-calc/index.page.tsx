607f23a4b1d04ce1be4b50b2e83603537d9d4538
Nathaniel Giabardo <nathaniel@Nathaniels-MacBook-Pro.local>
2023-06-30 18:13:10 +0800

    fixed learning hub doc urls

--- src/pages/learn-about-calc/index.page.tsx
+++ src/pages/learn-about-calc/index.page.tsx
@@ -2,9 +2,9 @@ import { Button, Flex, Grid, GridItem, Heading, Link, Stack, Text } from '@chakr
 import { getSidebarLayout } from '@components/Layout';
 
 export enum LearningHubLinks {
-  Dca = 'https://docs.calculated.fi/~/changes/KJ6lWJHrW4nR8itAdDCF/overview/calc-swap-tools/dollar-cost-averaging-dca',
-  DcaPlus = 'https://docs.calculated.fi/~/changes/KJ6lWJHrW4nR8itAdDCF/overview/calc-swap-tools/algorithm-dca+',
-  WeightedScale = 'https://docs.calculated.fi/~/changes/KJ6lWJHrW4nR8itAdDCF/overview/calc-swap-tools/weighted-scale-swaps',
+  Dca = 'https://docs.calculated.fi/deep-dive/calc-swap-tools/dollar-cost-averaging-dca',
+  DcaPlus = 'https://docs.calculated.fi/deep-dive/calc-swap-tools/algorithm-dca+',
+  WeightedScale = 'https://docs.calculated.fi/deep-dive/calc-swap-tools/weighted-scale-swaps',
   MoreAboutCalc = 'https://docs.calculated.fi/~/changes/KJ6lWJHrW4nR8itAdDCF/',
 }
 
