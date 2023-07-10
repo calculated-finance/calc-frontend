0ef190d60180282911a57ea5444f194b85220c89
Nathaniel Giabardo <nathaniel@Nathaniels-MacBook-Pro.local>
2023-07-10 21:11:41 +0800

    AvailableFunds Test

--- src/components/AvailableFunds/index.test.tsx
+++ src/components/AvailableFunds/index.test.tsx
@@ -1,21 +1,33 @@
 import { defaultDenom } from '@utils/defaultDenom';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import { Formik } from 'formik';
 import useBalance from '@hooks/useBalance';
 import useFiatPrice from '@hooks/useFiatPrice';
+import { useWallet } from '@hooks/useWallet';
 import { AvailableFunds } from '.';
 
 jest.mock('@hooks/useFiatPrice');
 jest.mock('@hooks/useBalance');
+jest.mock('@hooks/useWallet');
 
 describe('available funds', () => {
+  const mockResultingDenom = { ...defaultDenom, id: 'mockDenomId' };
+
+  (useFiatPrice as jest.Mock).mockReturnValue({
+    price: 23,
+  });
+
+  (useBalance as jest.Mock).mockReturnValue({
+    data: {
+      denom: 'ukuji',
+      amount: '1',
+    },
+  });
+
   it('shows connect wallet when not connected', () => {
-    const mockResultingDenom = { ...defaultDenom, id: 'mockDenomId' };
-    (useFiatPrice as jest.Mock).mockReturnValue({
-      price: 23,
-    });
-    (useBalance as jest.Mock).mockReturnValue({
-      price: 23,
+    (useWallet as jest.Mock).mockReturnValue({
+      connected: false,
     });
 
     render(
@@ -24,6 +36,22 @@ describe('available funds', () => {
       </Formik>,
     );
 
-    expect(screen.getByText(/Connect wallet/)).toBeInTheDocument();
+    expect(screen.getByText(/Connect wallet/)).toBeVisible();
+  });
+
+  it('does not show connect wallet when connected', () => {
+    (useWallet as jest.Mock).mockReturnValue({
+      connected: true,
+    });
+
+    render(
+      <Formik initialValues={{ initalDenom: 'ukuji' }} onSubmit={jest.fn()}>
+        <AvailableFunds denom={mockResultingDenom} />
+      </Formik>,
+    );
+
+    expect(screen.getByText(/None/)).toContainHTML('button');
+    expect(screen.getByText(/None/)).toBeInTheDocument();
+    expect(screen.getByText(/None/)).toBeDisabled();
   });
 });
