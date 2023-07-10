5903b81555647de7dcd599942d26babeaf0f670a
Nathaniel Giabardo <nathaniel@Nathaniels-MacBook-Pro.local>
2023-07-10 21:32:41 +0800

    updated comment

--- src/helpers/isStepOne.test.tsx
+++ src/helpers/isStepOne.test.tsx
@@ -1,33 +1,3 @@
-export const mockRouterAssets = {
-  isReady: true,
-  push: jest.fn(),
-  pathname: '/create-strategy/dca-out/assets',
-  query: { id: '1', chain: 'Kujira' },
-  events: {
-    on: jest.fn(),
-  },
-};
-jest.mock('next/router', () => ({
-  useRouter() {
-    return mockRouterAssets;
-  },
-}));
-
-export const mockRouterCustomise = {
-  isReady: true,
-  push: jest.fn(),
-  pathname: '/create-strategy/dca-out/customise',
-  query: { id: '1', chain: 'Kujira' },
-  events: {
-    on: jest.fn(),
-  },
-};
-jest.mock('next/router', () => ({
-  useRouter() {
-    return mockRouterCustomise;
-  },
-}));
-
 export function mockIsStepOne(path: string) {
   if (path.includes('assets')) {
     return true;
@@ -37,12 +7,12 @@ export function mockIsStepOne(path: string) {
 
 describe('checks if step one', () => {
   it('returns true if step one', () => {
-    const result = mockIsStepOne(mockRouterAssets.pathname);
+    const result = mockIsStepOne('assets');
     expect(result).toBe(true);
   });
 
   it('returns false if not step one', () => {
-    const result = mockIsStepOne(mockRouterCustomise.pathname);
+    const result = mockIsStepOne('customise');
     expect(result).toBe(false);
   });
 });
