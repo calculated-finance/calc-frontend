acca9cdfe57a488da9ca5db8bb0e662a8aa2e451
natecalc <125798762+natecalc@users.noreply.github.com>
2023-07-13 22:53:20 +0800

    Feat/get funds when none (#452)
    * added get funds button and logic - wip
    
    * added modal working - still need to write tests
    
    * availableFunds tests
    
    * added featureflag and wip for closing first modal
    
    * still wip
    
    * updated tests
    
    * fixed comment and test bug with doug - wip
    
    * fixed tests
    
    ---------
    
    Co-authored-by: Nathaniel Giabardo <nathaniel@Nathaniels-MacBook-Pro.local>
    Co-authored-by: Nathaniel Giabardo <nathaniel@192.168.1.25>

--- src/components/AvailableFunds/index.test.tsx
+++ src/components/AvailableFunds/index.test.tsx
@@ -2,56 +2,89 @@ import { defaultDenom } from '@utils/defaultDenom';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { Formik } from 'formik';
-import useBalance from '@hooks/useBalance';
-import useFiatPrice from '@hooks/useFiatPrice';
 import { useWallet } from '@hooks/useWallet';
+import useFiatPrice from '@hooks/useFiatPrice';
+import useBalance from '@hooks/useBalance';
 import { AvailableFunds } from '.';
 
 jest.mock('@hooks/useFiatPrice');
 jest.mock('@hooks/useBalance');
 jest.mock('@hooks/useWallet');
 
-describe('available funds', () => {
-  const mockResultingDenom = { ...defaultDenom, id: 'mockDenomId' };
+describe('available funds component', () => {
+  const mockResultingDenom = { ...defaultDenom, id: 'ukuji' };
 
-  (useFiatPrice as jest.Mock).mockReturnValue({
-    price: 23,
-  });
-
-  (useBalance as jest.Mock).mockReturnValue({
-    data: {
-      denom: 'ukuji',
-      amount: '1',
-    },
-  });
-
-  it('shows connect wallet when not connected', () => {
-    (useWallet as jest.Mock).mockReturnValue({
-      connected: false,
-    });
-
-    render(
-      <Formik initialValues={{ initalDenom: 'ukuji' }} onSubmit={jest.fn()}>
-        <AvailableFunds denom={mockResultingDenom} />
-      </Formik>,
-    );
-
-    expect(screen.getByText(/Connect wallet/)).toBeVisible();
-  });
-
-  it('does not show connect wallet when connected', () => {
+  beforeEach(() => {
     (useWallet as jest.Mock).mockReturnValue({
       connected: true,
     });
+    (useFiatPrice as jest.Mock).mockReturnValue({
+      price: 23,
+    });
+    (useBalance as jest.Mock).mockReturnValue({});
+  });
 
-    render(
-      <Formik initialValues={{ initalDenom: 'ukuji' }} onSubmit={jest.fn()}>
-        <AvailableFunds denom={mockResultingDenom} />
-      </Formik>,
-    );
+  describe('connect wallet button link', () => {
+    it('shows connect wallet when not connected', () => {
+      (useWallet as jest.Mock).mockReturnValue({
+        connected: false,
+      });
 
-    expect(screen.getByText(/None/)).toContainHTML('button');
-    expect(screen.getByText(/None/)).toBeInTheDocument();
-    expect(screen.getByText(/None/)).toBeDisabled();
+      render(
+        <Formik initialValues={{ initalDenom: 'ukuji' }} onSubmit={jest.fn()}>
+          <AvailableFunds denom={mockResultingDenom} />
+        </Formik>,
+      );
+
+      expect(screen.getByText(/Connect wallet/)).toBeInTheDocument();
+    });
+
+    it('does not show connect wallet when connected', () => {
+      render(
+        <Formik initialValues={{ initalDenom: 'ukuji' }} onSubmit={jest.fn()}>
+          <AvailableFunds denom={mockResultingDenom} />
+        </Formik>,
+      );
+
+      expect(screen.getByText(/None/)).toBeInTheDocument();
+    });
+  });
+
+  describe('Get funds button link', () => {
+    it('Display amount shows in available funds', () => {
+      (useBalance as jest.Mock).mockReturnValue({
+        data: {
+          denom: 'ukuji',
+          amount: '10000000',
+        },
+      });
+
+      render(
+        <Formik initialValues={{ initalDenom: 'ukuji' }} onSubmit={jest.fn()}>
+          <AvailableFunds denom={mockResultingDenom} />
+        </Formik>,
+      );
+      expect(screen.getByText(/9.995652/)).toBeInTheDocument();
+    });
+
+    it('Get funds button appears when no funds', () => {
+      render(
+        <Formik initialValues={{ initalDenom: 'ukuji' }} onSubmit={jest.fn()}>
+          <AvailableFunds denom={mockResultingDenom} />
+        </Formik>,
+      );
+      expect(screen.getByText(/Get funds/)).toBeInTheDocument();
+
+      expect(screen.getByTestId('get-funds-button')).toBeInTheDocument();
+    });
+
+    it('None appears when no funds', () => {
+      render(
+        <Formik initialValues={{ initalDenom: 'ukuji' }} onSubmit={jest.fn()}>
+          <AvailableFunds denom={mockResultingDenom} />
+        </Formik>,
+      );
+      expect(screen.getByText(/None/)).toBeInTheDocument();
+    });
   });
 });
--- src/components/AvailableFunds/index.tsx
+++ src/components/AvailableFunds/index.tsx
@@ -1,34 +1,209 @@
-import { Text, Button, Center, Tooltip } from '@chakra-ui/react';
-import useBalance, { getDisplayAmount } from '@hooks/useBalance';
+import {
+  Text,
+  Button,
+  Center,
+  Tooltip,
+  HStack,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  Stack,
+  ModalProps,
+  Box,
+  Grid,
+  GridItem,
+  Heading,
+  FormHelperText,
+  Image,
+} from '@chakra-ui/react';
+import useBalance from '@hooks/useBalance';
 import useFiatPrice from '@hooks/useFiatPrice';
 import { useField } from 'formik';
 import { createStrategyFeeInTokens } from '@helpers/createStrategyFeeInTokens';
 import { DenomInfo } from '@utils/DenomInfo';
+import OnRampModal from '@components/OnRampModalContent';
+import SquidModal from '@components/SquidModal';
+import { featureFlags } from 'src/constants';
 import { useWallet } from '@hooks/useWallet';
 import { useWalletModal } from '@hooks/useWalletModal';
+import { Coin } from '@cosmjs/proto-signing';
 
-export function AvailableFunds({ denom }: { denom: DenomInfo }) {
+interface GetFundsDetailsProps {
+  onSquidOpen: () => void;
+  onOnRampOpen: () => void;
+}
+
+interface GetFundsButtonProps {
+  onOpen: () => void;
+}
+
+function GetFundsDetails({ onSquidOpen, onOnRampOpen }: GetFundsDetailsProps) {
+  return (
+    <Box px={8} py={4} bg="abyss.200" fontSize="sm" borderRadius="xl" borderWidth={1} borderColor="slateGrey" w="full">
+      <Grid templateColumns="repeat(2, 1fr)" gap={2} alignItems="center" justifyItems="center" textAlign="center">
+        <GridItem>
+          <Center pb={4}>
+            <Image src="/images/squid-icon-logo-yellow.svg" alt="squid-logo" w={8} />
+          </Center>
+        </GridItem>
+        <GridItem>
+          <Center pb={4}>
+            <Image src="/images/kado.svg" alt="kado-logo" w={20} />
+          </Center>
+        </GridItem>
+
+        <GridItem>
+          <Heading size="xs">Squid Cross Chain Bridge</Heading>
+          <FormHelperText>Good for getting assets from other chains here.</FormHelperText>
+        </GridItem>
+        <GridItem mb={4}>
+          <Heading size="xs">Buy Crypto</Heading>
+          <FormHelperText>Good for getting crypto with cash.</FormHelperText>
+        </GridItem>
+        <GridItem>
+          <Button w={40} onClick={onSquidOpen}>
+            Move assets here
+          </Button>
+        </GridItem>
+        <GridItem>
+          <Button w={40} onClick={onOnRampOpen}>
+            Buy crypto
+          </Button>
+        </GridItem>
+      </Grid>
+    </Box>
+  );
+}
+
+function GetFundsModal({
+  onSquidOpen,
+  onOnRampOpen,
+  isOpen,
+  onClose,
+}: Omit<ModalProps, 'children'> & GetFundsDetailsProps) {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} size="xl">
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Get Funds</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Stack justify="center" gap={6} align="center">
+            <GetFundsDetails onSquidOpen={onSquidOpen} onOnRampOpen={onOnRampOpen} />
+          </Stack>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+}
+
+function GetFundsButton({ onOpen }: GetFundsButtonProps) {
+  return (
+    <HStack spacing={1}>
+      <Text fontSize="xs">None</Text>
+      {featureFlags.getFundsModalEnabled && (
+        <Button
+          size="xs"
+          data-testid="get-funds-button"
+          colorScheme="blue"
+          variant="link"
+          cursor="pointer"
+          onClick={onOpen}
+        >
+          Get funds
+        </Button>
+      )}
+    </HStack>
+  );
+}
+
+function AvailableFundsButton({
+  denom,
+  isLoading,
+  data,
+}: {
+  denom: DenomInfo;
+  isLoading: boolean;
+  data: Coin | undefined;
+}) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isOpen: isOnRampOpen, onClose: onOnRampClose, onOpen: onOnRampOpen } = useDisclosure();
+  const { isOpen: isSquidOpen, onClose: onSquidClose, onOpen: onSquidOpen } = useDisclosure();
+
   const { connected } = useWallet();
   const [, , helpers] = useField('initialDeposit');
-
+  const { setVisible } = useWalletModal();
   const { price } = useFiatPrice(denom);
-
-  const { data, isLoading } = useBalance(denom);
 
   const createStrategyFee = price ? Number(createStrategyFeeInTokens(price)) : 0;
   const balance = Number(data?.amount);
+  const displayAmount = denom.conversion(Math.max(balance - createStrategyFee, 0));
 
-  const displayAmount = getDisplayAmount(denom, Math.max(balance - createStrategyFee, 0));
-  const displayFee = getDisplayAmount(denom, createStrategyFee);
-
+  const handleConnect = () => {
+    setVisible(true);
+  };
   const handleClick = () => {
     helpers.setValue(displayAmount);
   };
 
-  const { setVisible } = useWalletModal();
-  const handleConnect = () => {
-    setVisible(true);
-  };
+  function handleOpen(onOpener: () => void) {
+    return () => {
+      onClose();
+      onOpener();
+    };
+  }
+
+  if (displayAmount) {
+    return (
+      <Button
+        size="xs"
+        isLoading={isLoading}
+        colorScheme="blue"
+        variant="link"
+        cursor="pointer"
+        isDisabled={!displayAmount}
+        onClick={handleClick}
+      >
+        {displayAmount}
+      </Button>
+    );
+  }
+
+  if (connected) {
+    return (
+      <>
+        <GetFundsButton onOpen={onOpen} />
+        <GetFundsModal
+          isOpen={isOpen}
+          onClose={onClose}
+          onSquidOpen={handleOpen(onSquidOpen)}
+          onOnRampOpen={handleOpen(onOnRampOpen)}
+        />
+        <SquidModal isOpen={isSquidOpen} onClose={onSquidClose} />
+        <OnRampModal isOpen={isOnRampOpen} onClose={onOnRampClose} />
+      </>
+    );
+  }
+
+  return (
+    <Button size="xs" colorScheme="blue" variant="link" cursor="pointer" onClick={handleConnect}>
+      Connect wallet
+    </Button>
+  );
+}
+
+export function AvailableFunds({ denom }: { denom: DenomInfo }) {
+  const { price } = useFiatPrice(denom);
+  const { data, isLoading } = useBalance(denom);
+  const createStrategyFee = price ? Number(createStrategyFeeInTokens(price)) : 0;
+  const balance = Number(data?.amount);
+
+  const displayFee = denom.conversion(createStrategyFee);
+
   return (
     <Center textStyle="body-xs">
       <Tooltip
@@ -39,23 +214,7 @@ export function AvailableFunds({ denom }: { denom: DenomInfo }) {
       >
         <Text mr={1}>Max*: </Text>
       </Tooltip>
-      {connected ? (
-        <Button
-          size="xs"
-          isLoading={isLoading}
-          colorScheme="blue"
-          variant="link"
-          cursor="pointer"
-          isDisabled={!displayAmount}
-          onClick={handleClick}
-        >
-          {displayAmount || 'None'}
-        </Button>
-      ) : (
-        <Button size="xs" colorScheme="blue" variant="link" cursor="pointer" onClick={handleConnect}>
-          Connect wallet
-        </Button>
-      )}
+      <AvailableFundsButton isLoading={isLoading} data={data} denom={denom} />
     </Center>
   );
 }
