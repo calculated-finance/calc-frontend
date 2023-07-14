6d0fd1be26b7498da3d6cbfc380761a6c370befd
Nathaniel Giabardo <nathaniel@Nathaniels-MacBook-Pro.local>
2023-07-14 18:23:47 +0800

    design done, working on functionality

--- src/pages/create-strategy/dca-in/assets/index.page.tsx
+++ src/pages/create-strategy/dca-in/assets/index.page.tsx
@@ -1,4 +1,24 @@
-import { Center, Stack, Tab, TabIndicator, TabList, Tabs, VStack } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Center,
+  HStack,
+  Icon,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  ModalProps,
+  Stack,
+  Text,
+  UseRadioProps,
+  VStack,
+  useDisclosure,
+  useRadio,
+  useRadioGroup,
+} from '@chakra-ui/react';
 import { getFlowLayout } from '@components/Layout';
 import { DcaInFormDataStep1, step1ValidationSchema } from 'src/models/DcaInFormData';
 import useDcaInForm from 'src/hooks/useDcaInForm';
@@ -18,26 +38,173 @@ import { TransactionType } from '@components/TransactionType';
 import Spinner from '@components/Spinner';
 import { useWallet } from '@hooks/useWallet';
 import Submit from '@components/Submit';
+import { QuestionOutlineIcon } from '@chakra-ui/icons';
+import { ChildrenProp } from '@helpers/ChildrenProp';
 import { StepOneConnectWallet } from '@components/StepOneConnectWallet';
 import { StrategyInfoProvider } from '../customise/useStrategyInfo';
 
-function AssetsTabSelectors() {
+function StrategyInfoModal({ isOpen, onClose }: Omit<ModalProps, 'children'>) {
   return (
-    <VStack pb={8} spacing={4}>
-      <Tabs position="relative" variant="unstyled">
-        <TabList>
-          <Tab>Buy strategies</Tab>
-          <Tab>Sell strategies</Tab>
-        </TabList>
-        <TabIndicator mt="-1.5px" height="2px" bg="blue.500" borderRadius="1px" />
-      </Tabs>
-      <Tabs variant="line" colorScheme="green" gap={24}>
-        <TabList>
-          <Tab>DCA</Tab>
-          <Tab>DCA+</Tab>
-          <Tab>Weighted Scale</Tab>
-        </TabList>
-      </Tabs>
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Performance free</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Stack spacing={8}>
+            <Stack spacing={1}>
+              <Text>Figure 1: A visual representation of a performance fee</Text>
+            </Stack>
+            <Stack spacing={1}>
+              <Text>Figure 2: A visual representation of when no performance fees are charged</Text>
+            </Stack>
+          </Stack>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+}
+
+function CategoryRadioCard({ ...props }: UseRadioProps & ChildrenProp) {
+  const { getInputProps, getRadioProps } = useRadio(props);
+  const { isOpen: isBuyOpen, onOpen: onBuyOpen, onClose: onBuyClose } = useDisclosure();
+  // const { isOpen: isSellOpen, onOpen: onSellOpen, onClose: onSellClose } = useDisclosure();
+
+  const input = getInputProps();
+  const checkbox = getRadioProps();
+
+  return (
+    <Box as="label">
+      <input {...input} />
+      <Box
+        {...checkbox}
+        cursor="pointer"
+        borderBottomWidth="1px"
+        borderRadius={0}
+        color="slategrey"
+        _checked={{
+          color: 'brand.200',
+          borderBottomColor: 'brand.200',
+        }}
+        fontSize={14}
+        fontWeight="bold"
+        textAlign="center"
+        px={1}
+        py={1}
+        w={44}
+        justifyContent="center"
+      >
+        <Center>
+          <HStack>
+            <Box> {props.children}</Box>
+            <Button variant="link" size="xs" color="slategrey" onClick={onBuyOpen}>
+              <Icon as={QuestionOutlineIcon} />
+            </Button>
+          </HStack>
+        </Center>
+      </Box>
+      <StrategyInfoModal isOpen={isBuyOpen} onClose={onBuyClose} />
+    </Box>
+  );
+}
+
+function StrategyTypeRadioCard({ ...props }: UseRadioProps & ChildrenProp) {
+  const { getInputProps, getRadioProps } = useRadio(props);
+
+  const input = getInputProps();
+  const checkbox = getRadioProps();
+
+  return (
+    <Box as="label">
+      <input {...input} />
+      <Box
+        {...checkbox}
+        cursor="pointer"
+        borderWidth="1px"
+        borderRadius="lg"
+        _checked={{
+          color: 'brand.200',
+          borderColor: 'brand.200',
+        }}
+        fontSize={14}
+        textAlign="center"
+        whiteSpace="nowrap"
+        w={32}
+        px={1}
+        py={1}
+      >
+        {props.children}
+      </Box>
+    </Box>
+  );
+}
+
+function AssetsTabSelectors() {
+  // const [group1Value, setGroup1Value] = useState('');
+  // const [group2Value, setGroup2Value] = useState('');
+
+  // const handleGroup1Change = (value: any) => {
+  //   setGroup1Value(value);
+  // };
+
+  // const handleGroup2Change = (value: any) => {
+  //   setGroup2Value(value);
+  // };
+
+  const buttonOptions = {
+    category: ['Buy strategies', 'Sell strategies'],
+    strategyType: {
+      displayName: ['DCA', 'DCA+', 'Weighted scale'],
+      pathName: ['dca', 'dca-plus', 'weighted-scale'],
+    },
+  };
+
+  const { getRootProps, getRadioProps } = useRadioGroup({
+    name: 'category',
+    defaultValue: 'Buy strategies',
+    onChange: console.log,
+  });
+  const { getRootProps: rootPropsStrategyType, getRadioProps: radioPropsStrategyType } = useRadioGroup({
+    name: 'strategy-type',
+    defaultValue: 'DCA',
+    onChange: (value) => {
+      const selectedPathName =
+        buttonOptions.strategyType.pathName[buttonOptions.strategyType.displayName.indexOf(value)];
+      console.log(selectedPathName);
+    },
+  });
+
+  const categoryGroup = getRootProps();
+  const strategyTypeGroup = rootPropsStrategyType();
+
+  const handleStrategyType = () => {
+    const url = `/create-strategy/${rootPropsStrategyType}-${getRootProps}/assets`;
+
+    console.log(url);
+  };
+
+  return (
+    <VStack spacing={4} pb={6}>
+      <HStack {...categoryGroup} spacing={8}>
+        {buttonOptions.category.map((value) => {
+          const radio = getRadioProps({ value });
+          return (
+            <CategoryRadioCard key={value} {...radio}>
+              {value}
+            </CategoryRadioCard>
+          );
+        })}
+      </HStack>
+      <HStack {...strategyTypeGroup} spacing={2}>
+        {buttonOptions.strategyType.displayName.map((value) => {
+          const radio = radioPropsStrategyType({ value });
+          return (
+            <StrategyTypeRadioCard key={value} {...radio}>
+              {value}
+            </StrategyTypeRadioCard>
+          );
+        })}
+      </HStack>
     </VStack>
   );
 }
@@ -74,6 +241,8 @@ function DcaIn() {
     initialDenom: state.step1.initialDenom,
     resultingDenom: state.step1.resultingDenom,
   };
+
+  console.log(initialValues);
 
   return (
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
