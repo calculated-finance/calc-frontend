6ce0396097a8943c1f4d4788e90f0c997ed73f28
Nathaniel Giabardo <nathaniel@192-168-1-112.tpgi.com.au>
2023-07-21 16:02:53 +1000

    added scroll bar to balance list when more than 5 assets

--- src/components/SpendableBalances.tsx
+++ src/components/SpendableBalances.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { Text, Divider, GridItem, Grid } from '@chakra-ui/react';
+import { Text, Divider, GridItem, Grid, Stack } from '@chakra-ui/react';
 import useBalances from '@hooks/useBalances';
 import getDenomInfo, { convertDenomFromCoin } from '@utils/getDenomInfo';
 import useFiatPrice from '@hooks/useFiatPrice';
@@ -46,31 +45,37 @@ function CoinBalanceWithFiat({ balance }: { balance: Coin }) {
 
 export function BalanceList({ balances = [], showFiat = false }: { balances: Coin[] | undefined; showFiat?: boolean }) {
   return (
-    <Grid templateRows="repeat(1, 1fr)" templateColumns="repeat(3, 1fr)" gap={2}>
-      <GridItem colSpan={1}>
-        <Text fontSize="xs" noOfLines={1}>
-          Balance
-        </Text>
-      </GridItem>
-      <GridItem colSpan={showFiat ? 1 : 2}>
-        <Text textStyle="body-xs">Asset</Text>
-      </GridItem>
-      {showFiat && (
+    <>
+      <Grid templateRows="repeat(1, 1fr)" templateColumns="repeat(3, 1fr)" gap={2}>
         <GridItem colSpan={1}>
-          <Text textStyle="body-xs">Fiat value</Text>
+          <Text fontSize="xs" noOfLines={1}>
+            Balance
+          </Text>
         </GridItem>
-      )}
-      <GridItem colSpan={3}>
-        <Divider />
-      </GridItem>
-      {balances?.map((balance: Coin) =>
-        showFiat ? (
-          <CoinBalanceWithFiat balance={balance} key={balance.denom} />
-        ) : (
-          <CoinBalance balance={balance} key={balance.denom} />
-        ),
-      )}
-    </Grid>
+        <GridItem colSpan={showFiat ? 1 : 2}>
+          <Text textStyle="body-xs">Asset</Text>
+        </GridItem>
+        <GridItem colSpan={3}>
+          <Divider />
+        </GridItem>
+      </Grid>
+      <Stack overflow="auto" maxH={220} hidden={balances.length < 5}>
+        <Grid templateRows="repeat(1, 1fr)" templateColumns="repeat(3, 1fr)" gap={2}>
+          {showFiat && (
+            <GridItem colSpan={1}>
+              <Text textStyle="body-xs">Fiat value</Text>
+            </GridItem>
+          )}
+          {balances?.map((balance: Coin) =>
+            showFiat ? (
+              <CoinBalanceWithFiat balance={balance} key={balance.denom} />
+            ) : (
+              <CoinBalance balance={balance} key={balance.denom} />
+            ),
+          )}
+        </Grid>
+      </Stack>
+    </>
   );
 }
 
