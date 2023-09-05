cb03124630cb1c22b74b776d00448be4c3ef0388
Nathaniel Giabardo <nathaniel@Nathaniels-MBP.gateway>
2023-09-05 16:24:04 +1000

    execution msg code

--- src/pages/control-desk/buildCreateVaultParamsControlDesk.ts
+++ src/pages/control-desk/buildCreateVaultParamsControlDesk.ts
@@ -1,4 +1,12 @@
+import { TransactionType } from '@components/TransactionType';
 import { ChainConfig } from '@helpers/chains';
+import {
+  buildCallbackDestinations,
+  getExecutionInterval,
+  getReceiveAmount,
+  getSlippageWithoutTrailingZeros,
+} from '@hooks/useCreateVault/buildCreateVaultParams';
+import { ExecutionIntervals } from '@models/ExecutionIntervals';
 import { DenomInfo } from '@utils/DenomInfo';
 import { ExecuteMsg } from 'kujira.js/lib/cjs/fin';
 import { Config } from 'kujira.js/lib/cjs/usk';
@@ -11,15 +19,15 @@ export type DestinationConfigControlDesk = {
 export type BuildCreateVaultControlDeskContext = {
   initialDenom: DenomInfo;
   resultingDenom: DenomInfo;
-  // timeInterval: {
-  //   increment: number;
-  //   interval: ExecutionIntervals;
-  // };
+  timeInterval: {
+    increment: number;
+    interval: ExecutionIntervals;
+  };
   targetAmount: number;
   startPrice?: number;
-  // swapAmount: number;
-  // priceThreshold?: number;
-  // transactionType: TransactionType;
+  swapAmount: number;
+  priceThreshold?: number;
+  transactionType: TransactionType;
   slippageTolerance: number;
   // swapAdjustment?: SwapAdjustment;
   destinationConfig: DestinationConfigControlDesk;
@@ -32,60 +40,39 @@ export function buildCreateVaultMsg(
     initialDenom,
     resultingDenom,
     startPrice,
-    // swapAmount,
-    // priceThreshold,
-    // transactionType,
+    swapAmount,
+    priceThreshold,
+    transactionType,
     slippageTolerance,
     destinationConfig,
+    timeInterval,
   }: BuildCreateVaultControlDeskContext,
 ): ExecuteMsg {
-  // const swapAdjustmentStrategy = swapAdjustment
-  //   ? buildWeightedScaleAdjustmentStrategy(
-  //       initialDenom,
-  //       swapAmount,
-  //       swapAdjustment.basePrice,
-  //       resultingDenom,
-  //       transactionType,
-  //       swapAdjustment.increaseOnly,
-  //       swapAdjustment.swapMultiplier,
-  //     )
-  //   : isDcaPlus
-  //   ? (buildDcaPlusAdjustmentStrategy(transactionType, fetchedConfig) as SwapAdjustmentStrategyParams)
-  //   : undefined;
-
-  // const performanceAssessmentStrategy = isDcaPlus
-  //   ? ('compare_to_standard_dca' as PerformanceAssessmentStrategyParams)
-  //   : undefined;
-
-  // const msg = {
-  //   create_vault: {
-  //     label: '',
-  //     time_interval: getExecutionInterval(timeInterval.interval, timeInterval.increment),
-  //     target_denom: resultingDenom.id,
-  //     swap_amount: getSwapAmount(initialDenom, swapAmount),
-  //     target_start_time_utc_seconds: timeTrigger && getStartTime(timeTrigger.startDate, timeTrigger.startTime),
-  //     minimum_receive_amount: priceThreshold
-  //       ? getReceiveAmount(initialDenom, swapAmount, priceThreshold, resultingDenom, transactionType)
-  //       : undefined,
-  //     slippage_tolerance: getSlippageWithoutTrailingZeros(slippageTolerance),
-  //     destinations: buildCallbackDestinations(
-  //       chainConfig,
-  //       destinationConfig.autoStakeValidator,
-  //       destinationConfig.recipientAccount,
-  //       destinationConfig.yieldOption,
-  //       destinationConfig.senderAddress,
-  //       destinationConfig.reinvestStrategyId,
-  //     ),
-  //     target_receive_amount: startPrice
-  //       ? getReceiveAmount(initialDenom, swapAmount, startPrice, resultingDenom, transactionType)
-  //       : undefined,
-  //     swap_adjustment_strategy: swapAdjustmentStrategy,
-  //     performance_assessment_strategy: performanceAssessmentStrategy,
-  //   },
-  // };
-
-  return {
-    launch: { msg: 'execute message' },
+  const msg = {
+    create_vault: {
+      label: '',
+      time_interval: getExecutionInterval(timeInterval.interval, timeInterval.increment),
+      target_denom: resultingDenom.id,
+      swap_amount: swapAmount,
+      minimum_receive_amount: priceThreshold
+        ? getReceiveAmount(initialDenom, swapAmount, priceThreshold, resultingDenom, transactionType)
+        : undefined,
+      slippage_tolerance: getSlippageWithoutTrailingZeros(slippageTolerance),
+      destinations: buildCallbackDestinations(
+        chainConfig,
+        destinationConfig.recipientAccount,
+        destinationConfig.senderAddress,
+        undefined,
+        '',
+        undefined,
+      ),
+      target_receive_amount: startPrice
+        ? getReceiveAmount(initialDenom, swapAmount, startPrice, resultingDenom, transactionType)
+        : undefined,
+    },
   };
-  // return msg;
+
+  return { launch: msg };
+
+  // return msg       Need to figure out how to make this work.
 }
