8a82f374d96bd1a7192b0a3ceeb639915499669c
Nate <nate@calculated.fi>
2023-09-18 12:49:26 +1000

    Revert "thinking w jimmy"
    This reverts commit 7091ec4aa2b5a3933a10760feef92f4448d99e7e.

--- src/pages/control-desk/Components/AddRecipientButton.tsx
+++ src/pages/control-desk/Components/AddRecipientButton.tsx
@@ -12,12 +12,11 @@ import {
   SimpleGrid,
   Spacer,
   VStack,
-} from "@chakra-ui/react";
-import { useChain } from "@hooks/useChain";
-import { Chains } from "@hooks/useChain/Chains";
-import { ErrorMessage, Field, FieldArray, useField } from "formik";
-import { FiMinusCircle, FiPlusCircle } from "react-icons/fi";
-
+} from '@chakra-ui/react';
+import { useChain } from '@hooks/useChain';
+import { Chains } from '@hooks/useChain/Chains';
+import { ErrorMessage, Field, FieldArray, useField } from 'formik';
+import { FiMinusCircle, FiPlusCircle } from 'react-icons/fi';
 
 const validateRecipientAccount = (value) => {
   if (!value) {
@@ -35,17 +34,12 @@ const validateAmount = (value) => {
   return undefined; // Return undefined for valid input
 };
 
-
 export function RecipientAccountControlDesk() {
-  const [field, meta,] = useField({ name: 'recipientArray' })
+  const { values } = useFormikContext<CtrlFormDataAll>();
 
-  const { chain } = useChain()
+  const { chain } = useChain();
 
-
-
-  console.log(meta)
-
-
+  console.log(meta);
 
   return (
     <FormControl isInvalid={Boolean(meta.touched && meta.error)}>
@@ -53,75 +47,82 @@ export function RecipientAccountControlDesk() {
       <FormLabel>Choose Account</FormLabel>
       <FormHelperText>This wallet address will be the one the funds are sent to.</FormHelperText>
       <SimpleGrid columns={2}>
-        <FormHelperText textAlign='left'>Recipient:</FormHelperText>
-        <FormHelperText textAlign='right' >Amount:</FormHelperText>
+        <FormHelperText textAlign="left">Recipient:</FormHelperText>
+        <FormHelperText textAlign="right">Amount:</FormHelperText>
       </SimpleGrid>
-
       <FieldArray
         name="recipientArray"
-        render={arrayHelpers => (
+        render={(arrayHelpers) => (
           <Box>
-
             {/* How does validation work on a 'per field' level. */}
-            {field?.value?.map((friend, index) =>
-
-
+            {field?.value?.map((friend, index) => (
               <VStack spacing={0} key={index}>
-
                 <InputGroup mb={2} key={index}>
-                  <HStack spacing={2} w='full'>
-                    {index === 0 ? undefined :
+                  <HStack spacing={2} w="full">
+                    {index === 0 ? undefined : (
                       <Button
                         size="xs"
-                        variant='ghost'
+                        variant="ghost"
                         width={4}
-                        bgColor='abyss.100'
+                        bgColor="abyss.100"
                         h={5}
                         borderRadius={6}
                         onClick={() => arrayHelpers.remove(index)}
-                        alignSelf='center'
+                        alignSelf="center"
                       >
                         <Icon as={FiMinusCircle} stroke="brand.200" width={3} height={3} />
                       </Button>
-                    }
-                    <Input as={Field} fontSize="sm" placeholder="Address" w='full' name={`recipientArray.${index}.recipientAccount`} validate={validateRecipientAccount} />
+                    )}
+                    <Input
+                      as={Field}
+                      fontSize="sm"
+                      placeholder="Address"
+                      w="full"
+                      name={`recipientArray.${index}.recipientAccount`}
+                      validate={validateRecipientAccount}
+                    />
 
-                    <Input as={Field} fontSize="sm" textAlign='right' placeholder="0" maxW={28} name={`recipientArray.${index}.amount`} validate={validateAmount} />
+                    <Input
+                      as={Field}
+                      fontSize="sm"
+                      textAlign="right"
+                      placeholder="0"
+                      maxW={28}
+                      name={`recipientArray.${index}.amount`}
+                      validate={validateAmount}
+                    />
                   </HStack>
                 </InputGroup>
 
                 <SimpleGrid columns={2} pb={2}>
-                  <FormErrorMessage textAlign='left'>
+                  <FormErrorMessage textAlign="left">
                     <ErrorMessage
                       name={`recipientArray.${index}.recipientAccount`}
                       className="recipientAccount-field-error"
-                      component='div'
+                      component="div"
                     />
                   </FormErrorMessage>
                   <Spacer />
-                  <FormErrorMessage textAlign='right'>
+                  <FormErrorMessage textAlign="right">
                     <ErrorMessage
                       name={`recipientArray.${index}.amount`}
                       className="amount-field-error"
-                      component='div'
+                      component="div"
                     />
-
                   </FormErrorMessage>
                 </SimpleGrid>
               </VStack>
-            )
-
-            }
+            ))}
             <FormHelperText>Ensure that this is a valid {Chains[chain]} address.</FormHelperText>
 
             <HStack pb={2}>
               <Button
                 size="xs"
-                variant='ghost'
+                variant="ghost"
                 leftIcon={<Icon as={FiPlusCircle} stroke="brand.200" width={3} height={3} />}
                 width={28}
-                bgColor='abyss.100'
-                fontSize='2xs'
+                bgColor="abyss.100"
+                fontSize="2xs"
                 h={5}
                 borderRadius={6}
                 onClick={() => arrayHelpers.push({})}
@@ -130,13 +131,9 @@ export function RecipientAccountControlDesk() {
               </Button>
               <Spacer />
             </HStack>
-          </Box>)}
-
+          </Box>
+        )}
       />
-
-
     </FormControl>
   );
 }
-
-
