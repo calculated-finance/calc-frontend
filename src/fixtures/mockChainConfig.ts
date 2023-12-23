1057f77fadf0753f87ef65976059a98318f09416
James Gaddum <james@calculated.fi>
2023-12-23 16:54:28 +1300

    update osmosis rpc url

--- src/fixtures/mockChainConfig.ts
+++
@@ -1,9 +0,0 @@
-import { ChainType } from '@helpers/chains';
-
-export const mockChainConfig = {
-  name: 'kaiyo-1',
-  chainType: ChainType.Cosmos,
-  contractAddress: 'contractAddress',
-  feeTakerAddress: 'feeTakerAddress',
-  autoCompoundStakingRewardsAddress: 'autoCompoundStakingRewardsAddress',
-};
