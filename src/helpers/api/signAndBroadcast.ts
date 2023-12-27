63ea11e08c7e137cb26981399f8798a697c98938
James Gaddum <james@calculated.fi>
2023-12-28 07:21:33 +1300

    improve dev rpc experience

--- src/helpers/api/signAndBroadcast.ts
+++ src/helpers/api/signAndBroadcast.ts
@@ -4,7 +4,6 @@ import * as Sentry from '@sentry/react';
 
 export async function signAndBroadcast(client: SigningCosmWasmClient, senderAddress: string, msgs: EncodeObject[]) {
   try {
-    // const gas = await client.simulate(senderAddress, msgs, 'memo');
     return await client.signAndBroadcast(senderAddress, msgs, 'auto');
   } catch (error: any) {
     if (error.toString().includes('transaction indexing is disabled')) {
