bde0747df1a021f5d9f4da820e7f0b21b56670ab
GadAlmighty <112296686+GadAlmighty@users.noreply.github.com>
2024-01-12 21:58:25 +1100

    up mulitplier from 1.3 to 1.6 (#556)

--- src/helpers/api/signAndBroadcast.ts
+++ src/helpers/api/signAndBroadcast.ts
@@ -4,7 +4,7 @@ import * as Sentry from '@sentry/react';
 
 export async function signAndBroadcast(client: SigningCosmWasmClient, senderAddress: string, msgs: EncodeObject[]) {
   try {
-    return await client.signAndBroadcast(senderAddress, msgs, 'auto');
+    return await client.signAndBroadcast(senderAddress, msgs, 1.6);
   } catch (error: any) {
     if (error.toString().includes('transaction indexing is disabled')) {
       Sentry.captureException(error, { tags: { page: 'signAndBroadcast' } });
