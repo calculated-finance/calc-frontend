d5e3003c79ad3cbec3e26fdf2d85a9d099241e7d
James Gaddum <james@calculated.fi>
2024-04-05 14:31:07 +1100

    add archway routes

--- src/fixtures/routes.ts
+++ src/fixtures/routes.ts
@@ -7,12 +7,232 @@ export const ARCHWAY_MAINNET: { [x: string]: { [x: string]: any[] } } = {
       { standard_hop_info: { pool: { id: 'standard_24' }, from_asset_index: 0 } },
       { stable_hop_info: { pool: { id: 'stable_11' }, from_asset_index: 1, to_asset_index: 0 } },
     ],
+    [ArchwayMainnetDenoms.AKT]: [
+      { stable_hop_info: { pool: { id: 'stable_6' }, from_asset_index: 0, to_asset_index: 1 } },
+      { standard_hop_info: { pool: { id: 'standard_5' }, from_asset_index: 0 } },
+      { standard_hop_info: { pool: { id: 'standard_3' }, from_asset_index: 1 } },
+      { stable_hop_info: { pool: { id: 'stable_5' }, from_asset_index: 1, to_asset_index: 0 } },
+    ],
+    [ArchwayMainnetDenoms.axlUSDC]: [
+      { stable_hop_info: { pool: { id: 'stable_6' }, from_asset_index: 0, to_asset_index: 1 } },
+      { standard_hop_info: { pool: { id: 'standard_15' }, from_asset_index: 1 } },
+    ],
+    [ArchwayMainnetDenoms.gravUSDC]: [
+      { stable_hop_info: { pool: { id: 'stable_6' }, from_asset_index: 0, to_asset_index: 1 } },
+      { standard_hop_info: { pool: { id: 'standard_23' }, from_asset_index: 1 } },
+      { standard_hop_info: { pool: { id: 'standard_12' }, from_asset_index: 0 } },
+      { stable_hop_info: { pool: { id: 'stable_12' }, from_asset_index: 2, to_asset_index: 0 } },
+    ],
+    [ArchwayMainnetDenoms.USDC]: [
+      { stable_hop_info: { pool: { id: 'stable_6' }, from_asset_index: 0, to_asset_index: 1 } },
+      { standard_hop_info: { pool: { id: 'standard_23' }, from_asset_index: 1 } },
+      { standard_hop_info: { pool: { id: 'standard_12' }, from_asset_index: 0 } },
+    ],
+    [ArchwayMainnetDenoms.gravUSDT]: [
+      { stable_hop_info: { pool: { id: 'stable_6' }, from_asset_index: 0, to_asset_index: 1 } },
+      { standard_hop_info: { pool: { id: 'standard_23' }, from_asset_index: 1 } },
+      { standard_hop_info: { pool: { id: 'standard_12' }, from_asset_index: 0 } },
+      { stable_hop_info: { pool: { id: 'stable_12' }, from_asset_index: 2, to_asset_index: 1 } },
+    ],
+  },
+  [ArchwayMainnetDenoms.AKT]: {
+    [ArchwayMainnetDenoms.ARCH]: [
+      { stable_hop_info: { pool: { id: 'stable_5' }, from_asset_index: 0, to_asset_index: 1 } },
+      { standard_hop_info: { pool: { id: 'standard_3' }, from_asset_index: 0 } },
+      { standard_hop_info: { pool: { id: 'standard_5' }, from_asset_index: 1 } },
+      { stable_hop_info: { pool: { id: 'stable_6' }, from_asset_index: 1, to_asset_index: 0 } },
+    ],
+    [ArchwayMainnetDenoms.ATOM]: [
+      { stable_hop_info: { pool: { id: 'stable_5' }, from_asset_index: 0, to_asset_index: 1 } },
+      { standard_hop_info: { pool: { id: 'standard_3' }, from_asset_index: 0 } },
+      { standard_hop_info: { pool: { id: 'standard_5' }, from_asset_index: 1 } },
+      { standard_hop_info: { pool: { id: 'standard_24' }, from_asset_index: 0 } },
+      { stable_hop_info: { pool: { id: 'stable_11' }, from_asset_index: 1, to_asset_index: 0 } },
+    ],
+    [ArchwayMainnetDenoms.axlUSDC]: [
+      { stable_hop_info: { pool: { id: 'stable_5' }, from_asset_index: 0, to_asset_index: 1 } },
+      { standard_hop_info: { pool: { id: 'standard_3' }, from_asset_index: 0 } },
+      { standard_hop_info: { pool: { id: 'standard_5' }, from_asset_index: 1 } },
+      { standard_hop_info: { pool: { id: 'standard_15' }, from_asset_index: 1 } },
+    ],
+    [ArchwayMainnetDenoms.gravUSDC]: [
+      { stable_hop_info: { pool: { id: 'stable_5' }, from_asset_index: 0, to_asset_index: 1 } },
+      { standard_hop_info: { pool: { id: 'standard_3' }, from_asset_index: 0 } },
+      { standard_hop_info: { pool: { id: 'standard_4' }, from_asset_index: 1 } },
+      { stable_hop_info: { pool: { id: 'stable_12' }, from_asset_index: 2, to_asset_index: 0 } },
+    ],
+    [ArchwayMainnetDenoms.USDC]: [
+      { stable_hop_info: { pool: { id: 'stable_5' }, from_asset_index: 0, to_asset_index: 1 } },
+      { standard_hop_info: { pool: { id: 'standard_3' }, from_asset_index: 0 } },
+      { standard_hop_info: { pool: { id: 'standard_4' }, from_asset_index: 1 } },
+    ],
+    [ArchwayMainnetDenoms.gravUSDT]: [
+      { stable_hop_info: { pool: { id: 'stable_5' }, from_asset_index: 0, to_asset_index: 1 } },
+      { standard_hop_info: { pool: { id: 'standard_3' }, from_asset_index: 0 } },
+      { standard_hop_info: { pool: { id: 'standard_4' }, from_asset_index: 1 } },
+      { stable_hop_info: { pool: { id: 'stable_12' }, from_asset_index: 2, to_asset_index: 1 } },
+    ],
   },
   [ArchwayMainnetDenoms.ATOM]: {
     [ArchwayMainnetDenoms.ARCH]: [
       { stable_hop_info: { pool: { id: 'stable_11' }, from_asset_index: 0, to_asset_index: 1 } },
       { standard_hop_info: { pool: { id: 'standard_24' }, from_asset_index: 1 } },
       { stable_hop_info: { pool: { id: 'stable_6' }, from_asset_index: 1, to_asset_index: 0 } },
+    ],
+    [ArchwayMainnetDenoms.AKT]: [
+      { stable_hop_info: { pool: { id: 'stable_11' }, from_asset_index: 0, to_asset_index: 1 } },
+      { standard_hop_info: { pool: { id: 'standard_27' }, from_asset_index: 1 } },
+      { standard_hop_info: { pool: { id: 'standard_6' }, from_asset_index: 0 } },
+      { standard_hop_info: { pool: { id: 'standard_3' }, from_asset_index: 1 } },
+      { stable_hop_info: { pool: { id: 'stable_5' }, from_asset_index: 1, to_asset_index: 0 } },
+    ],
+    [ArchwayMainnetDenoms.axlUSDC]: [
+      { stable_hop_info: { pool: { id: 'stable_11' }, from_asset_index: 0, to_asset_index: 1 } },
+      { standard_hop_info: { pool: { id: 'standard_24' }, from_asset_index: 1 } },
+      { standard_hop_info: { pool: { id: 'standard_15' }, from_asset_index: 1 } },
+    ],
+    [ArchwayMainnetDenoms.gravUSDC]: [
+      { stable_hop_info: { pool: { id: 'stable_11' }, from_asset_index: 0, to_asset_index: 1 } },
+      { standard_hop_info: { pool: { id: 'standard_27' }, from_asset_index: 1 } },
+      { standard_hop_info: { pool: { id: 'standard_12' }, from_asset_index: 0 } },
+      { stable_hop_info: { pool: { id: 'stable_12' }, from_asset_index: 2, to_asset_index: 0 } },
+    ],
+    [ArchwayMainnetDenoms.USDC]: [
+      { stable_hop_info: { pool: { id: 'stable_11' }, from_asset_index: 0, to_asset_index: 1 } },
+      { standard_hop_info: { pool: { id: 'standard_27' }, from_asset_index: 1 } },
+      { standard_hop_info: { pool: { id: 'standard_12' }, from_asset_index: 0 } },
+    ],
+    [ArchwayMainnetDenoms.gravUSDT]: [
+      { stable_hop_info: { pool: { id: 'stable_11' }, from_asset_index: 0, to_asset_index: 1 } },
+      { standard_hop_info: { pool: { id: 'standard_27' }, from_asset_index: 1 } },
+      { standard_hop_info: { pool: { id: 'standard_12' }, from_asset_index: 0 } },
+      { stable_hop_info: { pool: { id: 'stable_12' }, from_asset_index: 2, to_asset_index: 1 } },
+    ],
+  },
+  [ArchwayMainnetDenoms.axlUSDC]: {
+    [ArchwayMainnetDenoms.ARCH]: [
+      { standard_hop_info: { pool: { id: 'standard_15' }, from_asset_index: 0 } },
+      { stable_hop_info: { pool: { id: 'stable_6' }, from_asset_index: 1, to_asset_index: 0 } },
+    ],
+    [ArchwayMainnetDenoms.AKT]: [
+      { standard_hop_info: { pool: { id: 'standard_15' }, from_asset_index: 0 } },
+      { standard_hop_info: { pool: { id: 'standard_5' }, from_asset_index: 0 } },
+      { standard_hop_info: { pool: { id: 'standard_3' }, from_asset_index: 1 } },
+      { stable_hop_info: { pool: { id: 'stable_5' }, from_asset_index: 1, to_asset_index: 0 } },
+    ],
+    [ArchwayMainnetDenoms.ATOM]: [
+      { standard_hop_info: { pool: { id: 'standard_15' }, from_asset_index: 0 } },
+      { standard_hop_info: { pool: { id: 'standard_24' }, from_asset_index: 0 } },
+      { stable_hop_info: { pool: { id: 'stable_11' }, from_asset_index: 1, to_asset_index: 0 } },
+    ],
+    [ArchwayMainnetDenoms.gravUSDC]: [
+      { standard_hop_info: { pool: { id: 'standard_15' }, from_asset_index: 0 } },
+      { standard_hop_info: { pool: { id: 'standard_16' }, from_asset_index: 1 } },
+      { stable_hop_info: { pool: { id: 'stable_13' }, from_asset_index: 0, to_asset_index: 1 } },
+      { stable_hop_info: { pool: { id: 'stable_12' }, from_asset_index: 2, to_asset_index: 0 } },
+    ],
+    [ArchwayMainnetDenoms.USDC]: [
+      { standard_hop_info: { pool: { id: 'standard_15' }, from_asset_index: 0 } },
+      { standard_hop_info: { pool: { id: 'standard_16' }, from_asset_index: 1 } },
+      { stable_hop_info: { pool: { id: 'stable_13' }, from_asset_index: 0, to_asset_index: 1 } },
+    ],
+    [ArchwayMainnetDenoms.gravUSDT]: [
+      { standard_hop_info: { pool: { id: 'standard_15' }, from_asset_index: 0 } },
+      { standard_hop_info: { pool: { id: 'standard_16' }, from_asset_index: 1 } },
+      { stable_hop_info: { pool: { id: 'stable_13' }, from_asset_index: 0, to_asset_index: 1 } },
+      { stable_hop_info: { pool: { id: 'stable_12' }, from_asset_index: 2, to_asset_index: 1 } },
+    ],
+  },
+  [ArchwayMainnetDenoms.gravUSDC]: {
+    [ArchwayMainnetDenoms.AKT]: [
+      { stable_hop_info: { pool: { id: 'stable_12' }, from_asset_index: 0, to_asset_index: 2 } },
+      { standard_hop_info: { pool: { id: 'standard_4' }, from_asset_index: 0 } },
+      { standard_hop_info: { pool: { id: 'standard_3' }, from_asset_index: 1 } },
+      { stable_hop_info: { pool: { id: 'stable_5' }, from_asset_index: 1, to_asset_index: 0 } },
+    ],
+    [ArchwayMainnetDenoms.ARCH]: [
+      { stable_hop_info: { pool: { id: 'stable_12' }, from_asset_index: 0, to_asset_index: 2 } },
+      { stable_hop_info: { pool: { id: 'stable_13' }, from_asset_index: 1, to_asset_index: 0 } },
+      { standard_hop_info: { pool: { id: 'standard_16' }, from_asset_index: 0 } },
+      { stable_hop_info: { pool: { id: 'stable_6' }, from_asset_index: 1, to_asset_index: 0 } },
+    ],
+    [ArchwayMainnetDenoms.ATOM]: [
+      { stable_hop_info: { pool: { id: 'stable_12' }, from_asset_index: 0, to_asset_index: 2 } },
+      { standard_hop_info: { pool: { id: 'standard_12' }, from_asset_index: 1 } },
+      { standard_hop_info: { pool: { id: 'standard_27' }, from_asset_index: 0 } },
+      { stable_hop_info: { pool: { id: 'stable_11' }, from_asset_index: 1, to_asset_index: 0 } },
+    ],
+    [ArchwayMainnetDenoms.axlUSDC]: [
+      { stable_hop_info: { pool: { id: 'stable_12' }, from_asset_index: 0, to_asset_index: 2 } },
+      { stable_hop_info: { pool: { id: 'stable_13' }, from_asset_index: 1, to_asset_index: 0 } },
+      { standard_hop_info: { pool: { id: 'standard_16' }, from_asset_index: 0 } },
+      { standard_hop_info: { pool: { id: 'standard_15' }, from_asset_index: 1 } },
+    ],
+    [ArchwayMainnetDenoms.USDC]: [
+      { stable_hop_info: { pool: { id: 'stable_12' }, from_asset_index: 0, to_asset_index: 2 } },
+    ],
+    [ArchwayMainnetDenoms.gravUSDT]: [
+      { stable_hop_info: { pool: { id: 'stable_12' }, from_asset_index: 0, to_asset_index: 1 } },
+    ],
+  },
+  [ArchwayMainnetDenoms.USDC]: {
+    [ArchwayMainnetDenoms.AKT]: [
+      { standard_hop_info: { pool: { id: 'standard_4' }, from_asset_index: 0 } },
+      { standard_hop_info: { pool: { id: 'standard_3' }, from_asset_index: 1 } },
+      { stable_hop_info: { pool: { id: 'stable_5' }, from_asset_index: 1, to_asset_index: 0 } },
+    ],
+    [ArchwayMainnetDenoms.ARCH]: [
+      { stable_hop_info: { pool: { id: 'stable_13' }, from_asset_index: 1, to_asset_index: 0 } },
+      { standard_hop_info: { pool: { id: 'standard_16' }, from_asset_index: 0 } },
+      { stable_hop_info: { pool: { id: 'stable_6' }, from_asset_index: 1, to_asset_index: 0 } },
+    ],
+    [ArchwayMainnetDenoms.ATOM]: [
+      { standard_hop_info: { pool: { id: 'standard_12' }, from_asset_index: 1 } },
+      { standard_hop_info: { pool: { id: 'standard_27' }, from_asset_index: 0 } },
+      { stable_hop_info: { pool: { id: 'stable_11' }, from_asset_index: 1, to_asset_index: 0 } },
+    ],
+    [ArchwayMainnetDenoms.axlUSDC]: [
+      { stable_hop_info: { pool: { id: 'stable_13' }, from_asset_index: 1, to_asset_index: 0 } },
+      { standard_hop_info: { pool: { id: 'standard_16' }, from_asset_index: 0 } },
+      { standard_hop_info: { pool: { id: 'standard_15' }, from_asset_index: 1 } },
+    ],
+    [ArchwayMainnetDenoms.gravUSDC]: [
+      { stable_hop_info: { pool: { id: 'stable_12' }, from_asset_index: 2, to_asset_index: 0 } },
+    ],
+    [ArchwayMainnetDenoms.gravUSDT]: [
+      { stable_hop_info: { pool: { id: 'stable_12' }, from_asset_index: 2, to_asset_index: 1 } },
+    ],
+  },
+  [ArchwayMainnetDenoms.gravUSDT]: {
+    [ArchwayMainnetDenoms.AKT]: [
+      { stable_hop_info: { pool: { id: 'stable_12' }, from_asset_index: 1, to_asset_index: 2 } },
+      { standard_hop_info: { pool: { id: 'standard_4' }, from_asset_index: 0 } },
+      { standard_hop_info: { pool: { id: 'standard_3' }, from_asset_index: 1 } },
+      { stable_hop_info: { pool: { id: 'stable_5' }, from_asset_index: 1, to_asset_index: 0 } },
+    ],
+    [ArchwayMainnetDenoms.ARCH]: [
+      { stable_hop_info: { pool: { id: 'stable_12' }, from_asset_index: 1, to_asset_index: 2 } },
+      { stable_hop_info: { pool: { id: 'stable_13' }, from_asset_index: 1, to_asset_index: 0 } },
+      { standard_hop_info: { pool: { id: 'standard_16' }, from_asset_index: 0 } },
+      { stable_hop_info: { pool: { id: 'stable_6' }, from_asset_index: 1, to_asset_index: 0 } },
+    ],
+    [ArchwayMainnetDenoms.ATOM]: [
+      { stable_hop_info: { pool: { id: 'stable_12' }, from_asset_index: 1, to_asset_index: 2 } },
+      { standard_hop_info: { pool: { id: 'standard_12' }, from_asset_index: 1 } },
+      { standard_hop_info: { pool: { id: 'standard_27' }, from_asset_index: 0 } },
+      { stable_hop_info: { pool: { id: 'stable_11' }, from_asset_index: 1, to_asset_index: 0 } },
+    ],
+    [ArchwayMainnetDenoms.axlUSDC]: [
+      { stable_hop_info: { pool: { id: 'stable_12' }, from_asset_index: 1, to_asset_index: 2 } },
+      { stable_hop_info: { pool: { id: 'stable_13' }, from_asset_index: 1, to_asset_index: 0 } },
+      { standard_hop_info: { pool: { id: 'standard_16' }, from_asset_index: 0 } },
+      { standard_hop_info: { pool: { id: 'standard_15' }, from_asset_index: 1 } },
+    ],
+    [ArchwayMainnetDenoms.USDC]: [
+      { stable_hop_info: { pool: { id: 'stable_12' }, from_asset_index: 1, to_asset_index: 2 } },
+    ],
+    [ArchwayMainnetDenoms.gravUSDC]: [
+      { stable_hop_info: { pool: { id: 'stable_12' }, from_asset_index: 1, to_asset_index: 0 } },
     ],
   },
 };
