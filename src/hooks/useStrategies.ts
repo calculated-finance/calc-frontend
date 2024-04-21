4b692890d515ab39290eedf4f42b2a908c23d0e4
James Gaddum <james@calculated.fi>
2024-04-21 21:47:42 +1000

    generate addresses for fetching to avoid DAODAO issues

--- src/hooks/useStrategies.ts
+++ src/hooks/useStrategies.ts
@@ -3,10 +3,11 @@ import { queryClient } from 'src/pages/queryClient';
 import { Strategy } from '@models/Strategy';
 import { ChainContext } from '@cosmos-kit/core';
 import { ChainId } from '@models/ChainId';
-import { map, values } from 'rambda';
+import { any, map, values } from 'rambda';
 import { useChains } from '@cosmos-kit/react';
-import { getChainName, getDCAContractAddress } from '@helpers/chains';
+import { getAddressPrefix, getChainName, getDCAContractAddress } from '@helpers/chains';
 import { CHAINS, MAINNET_CHAINS } from 'src/constants';
+import { bech32 } from 'bech32';
 import getCalcClient from './useCalcClient/getClient/clients/cosmos';
 import useDenoms from './useDenoms';
 import { useWallet } from './useWallet';
@@ -14,32 +15,6 @@ import { useWallet } from './useWallet';
 const QUERY_KEY = 'get_vaults_by_address';
 
 export const invalidateStrategies = () => queryClient.invalidateQueries([QUERY_KEY]);
-
-// function useChainStrategies(chain: ChainContext) {
-//   const { connected } = useWallet();
-//   const { getDenomById } = useDenoms();
-
-//   return useQuery<Strategy[]>(
-//     [QUERY_KEY, chain.chain.chain_id, chain.address],
-//     async () => {
-//       const client = await chain.getCosmWasmClient();
-//       const calcClient = getCalcClient(
-//         chain.chain.chain_id as ChainId,
-//         getDCAContractAddress(chain.chain.chain_id as ChainId),
-//         client,
-//         getDenomById,
-//       );
-//       const userAddress = (await chain.getOfflineSigner().getAccounts())[0].address;
-//       return calcClient.fetchVaults(userAddress);
-//     },
-//     {
-//       enabled: connected && !!chain.chain.chain_id && !!chain.address,
-//       meta: {
-//         errorMessage: `Error fetching strategies for ${chain.address} on chain ${chain.chain.chain_name}`,
-//       },
-//     },
-//   );
-// }
 
 export function useStrategies() {
   const { address, connected } = useWallet();
@@ -59,7 +34,10 @@ export function useStrategies() {
           client,
           getDenomById,
         );
-        const userAddress = (await chain.getOfflineSigner().getAccounts())[0].address;
+        const userAddress = bech32.encode(
+          getAddressPrefix(chain.chain.chain_id as ChainId),
+          bech32.decode((await chain.getOfflineSigner().getAccounts())[0].address).words,
+        );
         return calcClient.fetchVaults(userAddress);
       };
 
@@ -72,13 +50,6 @@ export function useStrategies() {
       enabled: connected && !!address,
     },
   );
-
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  // const queries = chains.map((chain) => useChainStrategies(chain));
-  // const isLoading = any((strategy) => strategy.isLoading, queries);
-  // const strategies: Strategy[] | undefined = isLoading
-  // ? undefined
-  // : flatten(map((strategy) => strategy.data ?? [], queries));
 
   return {
     data: strategies,
