2a28e1757ff0abe1f25599eda3604cd03579f6ce
James Gaddum <james@calculated.fi>
2024-04-25 14:02:14 +1000

    remove discord link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,15 +5,12 @@ import { CALC_TELEGRAM_URL } from 'src/constants';
 
 function Footer() {
   return (
-    <Stack color="grey" direction="row" w="full" spacing={8}>
+    <Stack color="grey" direction="row" w="full" spacing={8} justifyContent="center">
       <Link target="_blank" href="https://twitter.com/CALC_Finance" rel="noopener noreferrer">
         <Icon as={TwitterIcon} stroke="slateGrey" />
       </Link>
       <Link target="_blank" href={CALC_TELEGRAM_URL} rel="noopener noreferrer">
         <Icon as={TelegramIcon} stroke="slateGrey" />
-      </Link>
-      <Link target="_blank" href="https://discord.gg/bJDj3zJhDH" rel="noopener noreferrer">
-        <Icon as={DiscordIcon} stroke="slateGrey" />
       </Link>
       <Link target="_blank" href="https://github.com/calculated-finance" rel="noopener noreferrer">
         <Icon as={GithubIcon} stroke="slateGrey" />
